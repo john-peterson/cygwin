1fef13634110fa3c3437d9b045122c7cb4c6f51d
jsm <jsm>
1999-06-28 23:02:40 +0000

    import gdb-1999-06-28 snapshot

--- gdb/testsuite/gdb.base/foll-fork.c
+++ gdb/testsuite/gdb.base/foll-fork.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 #ifdef PROTOTYPES
 void callee (int i)
@@ -9,8 +8,7 @@ void callee (i)
   int  i;
 #endif
 {
-  /* Any output corrupts GDB CLI expect strings.
-     printf("callee: %d\n", i);  */
+  printf("callee: %d\n", i);
 }
 
 #ifdef PROTOTYPES
@@ -23,18 +21,14 @@ main ()
   int  v = 5;
 
   pid = fork ();
-  if (pid == 0) /* set breakpoint here */
+  if (pid == 0)
     {
       v++;
       /* printf ("I'm the child!\n"); */
-      callee (getpid ());
     }
   else
     {
       v--;
       /* printf ("I'm the proud parent of child #%d!\n", pid); */
-      callee (getpid ());
     }
-
-  exit (0); /* at exit */
 }
