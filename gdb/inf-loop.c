4f7aa3bd557cb97329f833fbd789e77fdcee2d28
jsm <jsm>
1999-10-05 23:07:51 +0000

    import gdb-1999-10-04 snapshot

--- gdb/inf-loop.c
+++ gdb/inf-loop.c
@@ -1,12 +1,12 @@
 /* Handling of inferior events for the event loop for GDB, the GNU debugger.
-   Copyright (C) 1999-2013 Free Software Foundation, Inc.
+   Copyright 1999 Free Software Foundation, Inc.
    Written by Elena Zannoni <ezannoni@cygnus.com> of Cygnus Solutions.
 
    This file is part of GDB.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
+   the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -15,128 +15,68 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA. */
 
 #include "defs.h"
-#include "inferior.h"		/* For fetch_inferior_event.  */
-#include "target.h"             /* For enum inferior_event_type.  */
+#include "inferior.h"		/* For fetch_inferior_event. */
+#include "target.h"             /* For enum inferior_event_type. */
 #include "event-loop.h"
 #include "event-top.h"
 #include "inf-loop.h"
-#include "remote.h"
-#include "exceptions.h"
-#include "language.h"
-#include "gdbthread.h"
-#include "continuations.h"
-#include "interps.h"
-#include "top.h"
 
 static int fetch_inferior_event_wrapper (gdb_client_data client_data);
+static void complete_execution (void);
 
-/* General function to handle events in the inferior.  So far it just
+/* General function to handle events in the inferior. So far it just
    takes care of detecting errors reported by select() or poll(),
    otherwise it assumes that all is OK, and goes on reading data from
-   the fd.  This however may not always be what we want to do.  */
+   the fd. This however may not always be what we want to do. */
 void
 inferior_event_handler (enum inferior_event_type event_type, 
 			gdb_client_data client_data)
 {
-  struct cleanup *cleanup_if_error = make_bpstat_clear_actions_cleanup ();
-
   switch (event_type)
     {
+    case INF_ERROR:
+      printf_unfiltered ("error detected from target.\n");
+      target_async (NULL, 0);
+      pop_target ();
+      discard_all_continuations ();
+      do_exec_error_cleanups (ALL_CLEANUPS);
+      break;
+
     case INF_REG_EVENT:
       /* Use catch errors for now, until the inner layers of
 	 fetch_inferior_event (i.e. readchar) can return meaningful
 	 error status.  If an error occurs while getting an event from
-	 the target, just cancel the current command.  */
+	 the target, just get rid of the target. */
       if (!catch_errors (fetch_inferior_event_wrapper, 
 			 client_data, "", RETURN_MASK_ALL))
 	{
-	  bpstat_clear_actions ();
-	  do_all_intermediate_continuations (1);
-	  do_all_continuations (1);
-	  async_enable_stdin ();
+	  target_async (NULL, 0);
+	  pop_target ();
+	  discard_all_continuations ();
+	  do_exec_error_cleanups (ALL_CLEANUPS);
 	  display_gdb_prompt (0);
 	}
       break;
 
     case INF_EXEC_COMPLETE:
-      if (!non_stop)
-	{
-	  /* Unregister the inferior from the event loop.  This is done
-	     so that when the inferior is not running we don't get
-	     distracted by spurious inferior output.  */
-	  if (target_has_execution)
-	    target_async (NULL, 0);
-	}
-
-      /* Do all continuations associated with the whole inferior (not
-	 a particular thread).  */
-      if (!ptid_equal (inferior_ptid, null_ptid))
-	do_all_inferior_continuations (0);
-
-      /* If we were doing a multi-step (eg: step n, next n), but it
-	 got interrupted by a breakpoint, still do the pending
-	 continuations.  The continuation itself is responsible for
-	 distinguishing the cases.  The continuations are allowed to
-	 touch the inferior memory, e.g. to remove breakpoints, so run
-	 them before running breakpoint commands, which may resume the
-	 target.  */
-      if (non_stop
-	  && target_has_execution
-	  && !ptid_equal (inferior_ptid, null_ptid))
-	do_all_intermediate_continuations_thread (inferior_thread (), 0);
-      else
-	do_all_intermediate_continuations (0);
-
-      /* Always finish the previous command before running any
-	 breakpoint commands.  Any stop cancels the previous command.
-	 E.g. a "finish" or "step-n" command interrupted by an
-	 unrelated breakpoint is canceled.  */
-      if (non_stop
-	  && target_has_execution
-	  && !ptid_equal (inferior_ptid, null_ptid))
-	do_all_continuations_thread (inferior_thread (), 0);
-      else
-	do_all_continuations (0);
-
-      /* When running a command list (from a user command, say), these
-	 are only run when the command list is all done.  */
-      if (interpreter_async)
-	{
-	  volatile struct gdb_exception e;
-
-	  check_frame_language_change ();
-
-	  /* Don't propagate breakpoint commands errors.  Either we're
-	     stopping or some command resumes the inferior.  The user will
-	     be informed.  */
-	  TRY_CATCH (e, RETURN_MASK_ALL)
-	    {
-	      bpstat_do_actions ();
-	    }
-	  exception_print (gdb_stderr, e);
-	}
-      break;
-
-    case INF_EXEC_CONTINUE:
       /* Is there anything left to do for the command issued to
-         complete?  */
-
-      if (non_stop)
-	do_all_intermediate_continuations_thread (inferior_thread (), 0);
-      else
-	do_all_intermediate_continuations (0);
+         complete? */
+      do_all_continuations ();
+      /* Reset things after target has stopped for the async commands. */
+      complete_execution ();
       break;
 
+    case INF_QUIT_REQ:
     case INF_TIMER:
     default:
-      printf_unfiltered (_("Event type not recognized.\n"));
+      printf_unfiltered ("Event type not recognized.\n");
       break;
     }
-
-  discard_cleanups (cleanup_if_error);
 }
 
 static int 
@@ -144,4 +84,30 @@ fetch_inferior_event_wrapper (gdb_client_data client_data)
 {
   fetch_inferior_event (client_data);
   return 1;
+}
+
+/* Reset proper settings after an asynchronous command has finished.
+   If the execution command was in synchronous mode, register stdin
+   with the event loop, and reset the prompt. */
+
+static void
+complete_execution (void)
+{
+  target_executing = 0;
+  
+  /* Unregister the inferior from the event loop. This is done so that
+     when the inferior is not running we don't get distracted by
+     spurious inferior output. */
+  target_async (NULL, 0);
+
+  if (sync_execution)
+    {
+      do_exec_error_cleanups (ALL_CLEANUPS);
+      display_gdb_prompt (0);
+    }
+  else
+    {
+      if (exec_done_display_p)
+	printf_unfiltered ("completed.\n");
+    }
 }
