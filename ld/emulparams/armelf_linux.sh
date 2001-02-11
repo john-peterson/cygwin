9afb24e94b28a6401781b0284e3bbdba4ae93d8e
pb <pb>
2001-02-11 12:05:27 +0000

    2001-02-11  Philip Blundell  <philb@gnu.org>
    	* Makefile.am: Remove mention of earmelf_linux26.o.
    	* Makefile.in: Regenerate.
    	* emulparams/armelf_linux.sh (TEXT_START_ADDR): Set to 0x8000.
    	* emulparams/armelf_linux26.sh: Delete.
    	* configure.tgt: Remove mention of armelf_linux26 emulation.

--- ld/emulparams/armelf_linux.sh
+++ ld/emulparams/armelf_linux.sh
@@ -3,22 +3,14 @@ SCRIPT_NAME=elf
 OUTPUT_FORMAT="elf32-littlearm"
 BIG_OUTPUT_FORMAT="elf32-bigarm"
 LITTLE_OUTPUT_FORMAT="elf32-littlearm"
-MAXPAGESIZE="CONSTANT (MAXPAGESIZE)"
-COMMONPAGESIZE="CONSTANT (COMMONPAGESIZE)"
+MAXPAGESIZE=0x8000
 TEMPLATE_NAME=elf32
 EXTRA_EM_FILE=armelf
 GENERATE_SHLIB_SCRIPT=yes
-GENERATE_PIE_SCRIPT=yes
 
-DATA_START_SYMBOLS='PROVIDE (__data_start = .);';
-OTHER_TEXT_SECTIONS='*(.glue_7t) *(.glue_7) *(.vfp11_veneer) *(.v4_bx)'
+DATA_START_SYMBOLS='__data_start = . ;';
+OTHER_TEXT_SECTIONS='*(.glue_7t) *(.glue_7)'
 OTHER_BSS_SYMBOLS='__bss_start__ = .;'
-OTHER_BSS_END_SYMBOLS='_bss_end__ = . ; __bss_end__ = . ;'
-OTHER_END_SYMBOLS='__end__ = . ;'
-OTHER_SECTIONS='.note.gnu.arm.ident 0 : { KEEP (*(.note.gnu.arm.ident)) }'
+OTHER_BSS_END_SYMBOLS='_bss_end__ = . ; __bss_end__ = . ; __end__ = . ;'
 
 TEXT_START_ADDR=0x00008000
-TARGET2_TYPE=got-rel
-
-# ARM does not support .s* sections.
-NO_SMALL_DATA=yes
