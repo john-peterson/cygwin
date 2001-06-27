c52757c6962003fc83e58258c6599e10cb735cb8
amodra <amodra>
2001-06-27 13:07:05 +0000

    * emulparams/hppa64linux.sh: New file. 	* configure.tgt: hppa*64*-*-linux* uses hppa64linux.sh 	* Makefile.am (ALL_64_EMULATIONS): Add ehppa64linux.o 	(ehppa64linux.c): Add rule to make it. 	Run "make dep-am". 	* Makefile.in: Regenerate.

--- ld/emulparams/hppa64linux.sh
+++ ld/emulparams/hppa64linux.sh
@@ -1,10 +1,9 @@
 SCRIPT_NAME=elf
 ELFSIZE=64
 OUTPUT_FORMAT="elf64-hppa-linux"
-NO_REL_RELOCS=yes
 TEXT_START_ADDR=0x10000
 TARGET_PAGE_SIZE=0x10000
-MAXPAGESIZE="CONSTANT (MAXPAGESIZE)"
+MAXPAGESIZE=0x10000
 ARCH=hppa
 MACHINE=hppa2.0w
 ENTRY="main"
@@ -13,32 +12,21 @@ GENERATE_SHLIB_SCRIPT=yes
 
 # We really want multiple .stub sections, one for each input .text section,
 # but for now this is good enough.
-OTHER_READONLY_SECTIONS="
-  .PARISC.unwind ${RELOCATING-0} : { *(.PARISC.unwind) }"
+OTHER_READONLY_SECTIONS='.PARISC.unwind : { *(.PARISC.unwind) } '
 
 # The PA64 ELF port treats .plt sections differently than most.  We also have
 # to create a .opd section.  What most systems call the .got, we call the .dlt
-OTHER_READWRITE_SECTIONS="
-  .opd          ${RELOCATING-0} : { *(.opd) }
-  ${RELOCATING+PROVIDE (__gp = .);}
-  .plt          ${RELOCATING-0} : { *(.plt) }
-  .dlt          ${RELOCATING-0} : { *(.dlt) }"
+OTHER_READWRITE_SECTIONS='.opd : { *(.opd) } PROVIDE (__gp = .); .plt : { *(.plt) } .dlt : { *(.dlt) }'
 
-# The PA64 ELF port has an additional huge bss section.
-OTHER_BSS_SECTIONS=".hbss         ${RELOCATING-0} : { *(.hbss) }"
-
-#OTHER_SYMBOLS='PROVIDE (__TLS_SIZE = SIZEOF (.tbss));'
-OTHER_SYMBOLS='
-  PROVIDE (__TLS_SIZE = 0);
-  PROVIDE (__TLS_INIT_SIZE = 0);
-  PROVIDE (__TLS_INIT_START = 0);
-  PROVIDE (__TLS_INIT_A = 0);
-  PROVIDE (__TLS_PREALLOC_DTV_A = 0);'
+# The PA64 ELF port has two additional bss sections. huge bss and thread bss.
+# Make sure they end up in the appropriate location.  We also have to set
+# __TLS_SIZE to the size of the thread bss section.
+OTHER_BSS_SECTIONS='.hbss : { *(.hbss) } .tbss : { *(.tbss) }'
+#OTHER_BSS_END_SYMBOLS='PROVIDE (__TLS_SIZE = SIZEOF (.tbss));'
+OTHER_BSS_END_SYMBOLS='PROVIDE (__TLS_SIZE = 0);'
 
 # HPs use .dlt where systems use .got.  Sigh.
-OTHER_GOT_RELOC_SECTIONS="
-  .rela.dlt     ${RELOCATING-0} : { *(.rela.dlt) }
-  .rela.opd     ${RELOCATING-0} : { *(.rela.opd) }"
+OTHER_GOT_RELOC_SECTIONS='.rela.dlt : { *(.rela.dlt) }'
 
 # We're not actually providing a symbol anymore (due to the inability to be
 # safe in regards to shared libraries). So we just allocate the hunk of space
@@ -46,7 +34,6 @@ OTHER_GOT_RELOC_SECTIONS="
 DATA_START_SYMBOLS='. += 16;'
 
 DATA_PLT=
-PLT_BEFORE_GOT=
 
 # .dynamic should be at the start of the .text segment.
 TEXT_DYNAMIC=
