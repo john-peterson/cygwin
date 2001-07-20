938be542878ce8084c34cb4918dd562a67650f24
obrien <obrien>
2001-07-20 02:22:17 +0000

    2001-07-19  David O'Brien  <obrien@FreeBSD.org> 	* lbasename.c: New file. 	* libiberty.h: Add lbasename.
    Approved by:  Philip Blundell <philb@gnu.org>
                  <E15K1Xn-00032C-00@kings-cross.london.uk.eu.org>
    
    
    	* emultempl/elf32.em: Do not assuming that contents of the buffer
    	returned from basename function will remain unchanged accross other
    	function calls.
    
    Approved by:  Philip Blundell <philb@gnu.org>
                  <E15MO4s-00017V-00@kings-cross.london.uk.eu.org>

--- libiberty/lbasename.c
+++ libiberty/lbasename.c
@@ -1,6 +1,6 @@
 /* Libiberty basename.  Like basename, but is not overridden by the
    system C library.
-   Copyright (C) 2001, 2002, 2010 Free Software Foundation, Inc.
+   Copyright (C) 2001 Free Software Foundation, Inc.
 
 This file is part of the libiberty library.
 Libiberty is free software; you can redistribute it and/or
@@ -15,70 +15,75 @@ Library General Public License for more details.
 
 You should have received a copy of the GNU Library General Public
 License along with libiberty; see the file COPYING.LIB.  If
-not, write to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
-Boston, MA 02110-1301, USA.  */
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  */
 
 /*
+NAME
+	lbasename -- return pointer to last component of a pathname
 
-@deftypefn Replacement {const char*} lbasename (const char *@var{name})
+SYNOPSIS
+	char *lbasename (const char *name)
 
-Given a pointer to a string containing a typical pathname
-(@samp{/usr/src/cmd/ls/ls.c} for example), returns a pointer to the
-last component of the pathname (@samp{ls.c} in this case).  The
-returned pointer is guaranteed to lie within the original
-string.  This latter fact is not true of many vendor C
-libraries, which return special strings or modify the passed
-strings for particular input.
+DESCRIPTION
+	Given a pointer to a string containing a typical pathname
+	(/usr/src/cmd/ls/ls.c for example), returns a pointer to the
+	last component of the pathname ("ls.c" in this case).  The
+	returned pointer is guaranteed to lie within the original
+	string.  This latter fact is not true of many vendor C
+	libraries, which return special strings or modify the passed
+	strings for particular input.
 
-In particular, the empty string returns the same empty string,
-and a path ending in @code{/} returns the empty string after it.
-
-@end deftypefn
-
+	In particular, the empty string returns the same empty string,
+	and a path ending in '/' returns the empty string after it.
 */
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
 #include "ansidecl.h"
 #include "libiberty.h"
 #include "safe-ctype.h"
-#include "filenames.h"
 
-const char *
-unix_lbasename (const char *name)
+#ifndef DIR_SEPARATOR
+#  define DIR_SEPARATOR '/'
+#endif
+
+#if defined (_WIN32) || defined (__MSDOS__) \
+    || defined (__DJGPP__) || defined (__OS2__)
+#  define HAVE_DOS_BASED_FILE_SYSTEM
+#  ifndef DIR_SEPARATOR_2 
+#    define DIR_SEPARATOR_2 '\\'
+#  endif
+#endif
+
+/* Define IS_DIR_SEPARATOR. VMS uses '::', ':', '[...]' and '<...>' to
+   separate the different components of a file specification.  It's a
+   bit of a stretch to call ':', ']' and '>' directory separators, so
+   just define the test to find the file name component.  */
+#ifdef VMS
+#  define IS_DIR_SEPARATOR(ch) ((ch) == ':' || (ch) == ']' || (ch) == '>')
+#else
+#  ifndef DIR_SEPARATOR_2
+#    define IS_DIR_SEPARATOR(ch) ((ch) == DIR_SEPARATOR)
+#  else
+#    define IS_DIR_SEPARATOR(ch) \
+	(((ch) == DIR_SEPARATOR) || ((ch) == DIR_SEPARATOR_2))
+#  endif
+#endif
+
+char *
+lbasename (name)
+     const char *name;
 {
   const char *base;
 
-  for (base = name; *name; name++)
-    if (IS_UNIX_DIR_SEPARATOR (*name))
-      base = name + 1;
-
-  return base;
-}
-
-const char *
-dos_lbasename (const char *name)
-{
-  const char *base;
-
+#if defined (HAVE_DOS_BASED_FILE_SYSTEM)
   /* Skip over a possible disk name.  */
   if (ISALPHA (name[0]) && name[1] == ':') 
     name += 2;
+#endif
 
   for (base = name; *name; name++)
-    if (IS_DOS_DIR_SEPARATOR (*name))
+    if (IS_DIR_SEPARATOR (*name))
       base = name + 1;
 
-  return base;
-}
-
-const char *
-lbasename (const char *name)
-{
-#if defined (HAVE_DOS_BASED_FILE_SYSTEM)
-  return dos_lbasename (name);
-#else
-  return unix_lbasename (name);
-#endif
+  return (char *) base;
 }
