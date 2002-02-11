11e439aa5644acf595a00cf3a08b73088df15b27
aoliva <aoliva>
2002-02-11 06:13:46 +0000

    * ld-sh/shared-1.d: Fix relocation addend.

--- ld/testsuite/ld-sh/shared-1.d
+++ ld/testsuite/ld-sh/shared-1.d
@@ -1,7 +1,7 @@
 #source: ldr1.s
 #source: ldr2.s
 #as: -little
-#ld: -shared -EL -z nocombreloc
+#ld: -shared -EL
 #readelf: -r -x4 -x5
 #target: sh*-*-elf
 
@@ -11,13 +11,12 @@
 # about how, when, where and which addends to use.  A DSO must have the
 # same value in the addend as in the data, so either can be used.
 
-Relocation section '\.rela\.text' at offset 0x[0-9a-f]+ contains 1 entries:
-.*
-0000019c +[0-9a-f]+ R_SH_RELATIVE +1a0
+Relocation section '\.rela\.text' at offset 0x328 contains 1 entries:
+ Offset     Info    Type            Symbol's Value  Symbol's Name          Addend
+0000033c  000000a5 R_SH_RELATIVE                                          00000340
 
 Hex dump of section '\.rela\.text':
-  0x00000188 9c010000 a5000000 a0010000 .*
+  0x00000328          00000340 000000a5 0000033c .*
 
 Hex dump of section '\.text':
-.*
-  0x00000194 09000900 09000900 a0010000 .*
+  0x00000334          00000340 00090009 00090009 .*
