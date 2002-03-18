fb23e6e555a14bee952e8eadceb3f646c64c2c71
hubicka <hubicka>
2002-03-18 17:08:59 +0000

    * cpu-i386.c (bfd_x86_64_arch): Rename to "i386:x86-64"
    	* configure.tgt (x86_64-*-linux-gnu*): Configure i386 as native.
    
    	* elf_x86_64.sh (ARCH): Set to i386:x86-64
    	set libraries to default to lib64 paths.

--- ld/emulparams/elf_x86_64.sh
+++ ld/emulparams/elf_x86_64.sh
@@ -1,39 +1,43 @@
-. ${srcdir}/emulparams/plt_unwind.sh
 SCRIPT_NAME=elf
 ELFSIZE=64
 OUTPUT_FORMAT="elf64-x86-64"
-NO_REL_RELOCS=yes
 TEXT_START_ADDR=0x400000
-MAXPAGESIZE="CONSTANT (MAXPAGESIZE)"
-COMMONPAGESIZE="CONSTANT (COMMONPAGESIZE)"
+MAXPAGESIZE=0x100000
+NONPAGED_TEXT_START_ADDR=0x400000
 ARCH="i386:x86-64"
 MACHINE=
+NOP=0x90909090
 TEMPLATE_NAME=elf32
 GENERATE_SHLIB_SCRIPT=yes
-GENERATE_PIE_SCRIPT=yes
 NO_SMALL_DATA=yes
-LARGE_SECTIONS=yes
-LARGE_BSS_AFTER_BSS=
-SEPARATE_GOTPLT="SIZEOF (.got.plt) >= 24 ? 24 : 0"
-IREL_IN_PLT=
 
 if [ "x${host}" = "x${target}" ]; then
   case " $EMULATION_LIBPATH " in
     *" ${EMULATION_NAME} "*)
-      NATIVE=yes
+      # Native, and default or emulation requesting LIB_PATH.
+
+      # Linux modify the default library search path to first include
+      # a 64-bit specific directory.
+      case "$target" in
+        x86_64*-linux*)
+          suffix=64 ;;
+      esac
+
+      if [ -n "${suffix}" ]; then
+
+	LIB_PATH=/lib${suffix}:/lib
+	LIB_PATH=${LIB_PATH}:/usr/lib${suffix}:/usr/lib
+	if [ -n "${NATIVE_LIB_DIRS}" ]; then
+	  LIB_PATH=${LIB_PATH}:`echo ${NATIVE_LIB_DIRS} | sed s_:_${suffix}:_g`${suffix}:${NATIVE_LIB_DIRS}
+	fi
+	if [ "${libdir}" != /usr/lib ]; then
+	  LIB_PATH=${LIB_PATH}:${libdir}${suffix}:${libdir}
+	fi
+	if [ "${libdir}" != /usr/local/lib ]; then
+	  LIB_PATH=${LIB_PATH}:/usr/local/lib${suffix}:/usr/local/lib
+	fi
+
+      fi
+    ;;
   esac
 fi
-
-# Linux/Solaris modify the default library search path to first include
-# a 64-bit specific directory.
-case "$target" in
-  x86_64*-linux*|i[3-7]86-*-linux-*)
-    case "$EMULATION_NAME" in
-      *64*) LIBPATH_SUFFIX=64 ;;
-    esac
-    ;;
-  *-*-solaris2*)
-    LIBPATH_SUFFIX=/amd64
-    ELF_INTERPRETER_NAME=\"/lib/amd64/ld.so.1\"
-  ;;
-esac
