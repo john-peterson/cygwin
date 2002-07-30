9533d1c040f7d1f3fea078658d0b1a306f85618b
drow <drow>
2002-07-30 18:20:01 +0000

    Merge from mainline:         2002-07-29  Chris Demetriou  <cgd@broadcom.com>         * gas/mips/elf_ase_mips16.d: New file to test ELF MIPS16 ASE marking.         * gas/mips/elf_ase_mips16.s: Likewise.         * gas/mips/mips.exp: Run the new test.

--- gas/testsuite/gas/mips/elf_ase_mips16.d
+++ gas/testsuite/gas/mips/elf_ase_mips16.d
@@ -1,8 +1,8 @@
 # name: ELF MIPS16 ASE markings
-# source: empty.s
+# source: elf_ase_mips16.s
 # objdump: -p
-# as: -32 -mips16
+# as: -mips16
 
 .*:.*file format.*mips.*
-!private flags = .*mips16.*
+private flags = [0-9a-f]*[4-7c-f]......: .*[[,]mips16[],].*
 
