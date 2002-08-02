706b24a605053372de06756e7e655b950361fdff
drow <drow>
2002-08-02 02:03:08 +0000

    Merge from mainline:         2002-08-01  Richard Sandiford  <rsandifo@redhat.com>         * config/tc-mips.c (tc_gen_reloc): Extend GP-relative addend         handling to BFD_RELOC_MIPS16_GPREL.
            Merge from mainline:
            2002-08-01  Richard Sandiford  <rsandifo@redhat.com>
            * gas/mips/elf-rel6.[sd]: New test.
            * gas/mips/mips.exp: Run it.

--- gas/testsuite/gas/mips/elf-rel6.d
+++ gas/testsuite/gas/mips/elf-rel6.d
@@ -1,6 +1,5 @@
 #objdump: -dr --prefix-addresses
 #name: MIPS ELF reloc 6
-#as: -32
 
 .*: +file format elf.*mips.*
 
@@ -9,7 +8,3 @@ Disassembly of section \.text:
 			0: R_MIPS16_GPREL	bar
 0+04 <.*> lb	v0,1\(v1\)
 			4: R_MIPS16_GPREL	bar
-0+08 <.*> lb	v0,4660\(v1\)
-			8: R_MIPS16_GPREL	bar
-0+0c <[^>]*> nop
-0+0e <[^>]*> nop
