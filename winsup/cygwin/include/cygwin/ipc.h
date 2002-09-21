29b5ac66522b4422616188929e83ef5e50b58989
scottc <scottc>
2002-09-21 13:11:43 +0000

    Merged changes from HEAD

--- winsup/cygwin/include/cygwin/ipc.h
+++ winsup/cygwin/include/cygwin/ipc.h
@@ -1,6 +1,6 @@
 /* sys/ipc.h
 
-   Copyright 2001, 2002 Red Hat Inc.
+   Copyright 2001 Red Hat Inc. 
    Written by Robert Collins <rbtcollins@hotmail.com>
 
 This file is part of Cygwin.
@@ -9,63 +9,45 @@ This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
 details. */
 
-#ifndef _CYGWIN_IPC_H
-#define _CYGWIN_IPC_H
-
-#include <sys/types.h>
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-struct ipc_perm
-{
-  uid_t  uid;			/* Owner's user ID. */
-  gid_t  gid;			/* Owner's group ID. */
-  uid_t  cuid;			/* Creator's user ID. */
-  gid_t  cgid;			/* Creator's group ID. */
-  mode_t mode;			/* Read/write permission. */
-  key_t  key;
-  unsigned short seq;
-};
+#ifndef _SYS_IPC_H
+#define _SYS_IPC_H
 
-/* Mode bits:
+/* sys/types must be included before sys/ipc.h. We aren't meant to automatically 
+ * include it however 
  */
-#define IPC_CREAT  0x0200	/* Create entry if key does not exist. */
-#define IPC_EXCL   0x0400	/* Fail if key exists. */
-#define IPC_NOWAIT 0x0800	/* Error if request must wait. */
-#ifdef _KERNEL
-#define IPC_KEY_IS_SHMID 0x1000	/* Used in shmget when called from shmat. */
-#endif
 
-/* Keys:
+struct ipc_perm {
+  uid_t  uid;
+  gid_t  gid;
+  uid_t  cuid;
+  gid_t  cgid;
+  mode_t mode;
+}; 
+
+/* the mode flags used with the _get functions use the low order 9 bits for a mode 
+ * request
  */
-#define IPC_PRIVATE ((key_t) 0)	/* Private key. */
+#define IPC_CREAT  0x0200
+#define IPC_EXCL   0x0400
+#define IPC_NOWAIT 0x0800
 
-/* Control commands:
- */
-#define IPC_RMID 0x1000		/* Remove identifier. */
-#define IPC_SET  0x1001		/* Set options. */
-#define IPC_STAT 0x1002		/* Get options. */
-#ifdef _KERNEL
-#define IPC_INFO 0x1003		/* For ipcs(8). */
-#endif
+/* this is a value that will _never_ be a valid key from ftok */
+#define IPC_PRIVATE -2
 
-#ifdef _KERNEL
-#define IPCID_TO_IX(id)         ((id) & 0xffff)
-#define IPCID_TO_SEQ(id)        (((id) >> 16) & 0xffff)
-#define IXSEQ_TO_IPCID(ix,perm) (((perm.seq) << 16) | (ix & 0xffff))
+/* ctl commands 1000-1fff is ipc reserved */
+#define IPC_RMID 0x1003
+#define IPC_SET  0x1002
+#define IPC_STAT 0x1001
 
-#define IPC_R           000400  /* read permission */
-#define IPC_W           000200  /* write/alter permission */
-#define IPC_M           010000  /* permission to change control info */
-#endif
+key_t ftok(const char *, int);
 
-key_t ftok (const char *path, int id);
+#endif /* _SYS_IPC_H */
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* _CYGWIN_IPC_H */
--- winsup/cygwin/include/cygwin/shm.h
+++ winsup/cygwin/include/cygwin/shm.h
@@ -1,6 +1,6 @@
 /* sys/shm.h
 
-   Copyright 2001, 2002 Red Hat Inc.
+   Copyright 2001 Red Hat Inc. 
    Written by Robert Collins <rbtcollins@hotmail.com>
 
 This file is part of Cygwin.
@@ -9,98 +9,81 @@ This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
 details. */
 
-#ifndef _CYGWIN_SHM_H
-#define _CYGWIN_SHM_H
-
-#include <cygwin/ipc.h>
-#include <sys/cygwin.h>
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-/* Segment low boundary address multiple.
- *
- * 64 Kb was hardcoded for x86. MS states this may change so the constant
- * expression is replaced by a function call returning the correct value. */
-#define SHMLBA	(cygwin_internal (CW_GET_SHMLBA))
+#ifndef _SYS_SHM_H
+#define _SYS_SHM_H
 
-/* Shared memory operation flags:
+#include <cygwin/ipc.h>
+
+#define SHM_RDONLY 1
+/* 64 Kb was hardcoded for x86. MS states this may change, but we need it in the header
+ * file.
  */
-#define SHM_RDONLY 0x01		/* Attach read-only (else read-write). */
-#define SHM_RND    0x02		/* Round attach address to SHMLBA. */
+#define SHMLBA     65536
+#define SHM_RND	   1
 
-#ifdef _KERNEL
-/* Command definitions for the semctl () function:
- */
-#define SHM_STAT   0x4000	/* For ipcs(8) */
-#define SHM_INFO   0x4001	/* For ipcs(8) */
-#endif /* _KERNEL */
+typedef long int shmatt_t;
 
-/* Unsigned integer used for the number of current attaches.
- */
-typedef unsigned int shmatt_t;
+#if defined(__INSIDE_CYGWIN__) && defined(__cplusplus)
 
-struct shmid_ds
-{
-  struct ipc_perm    shm_perm;	/* Operation permission structure. */
-  size_t             shm_segsz;	/* Size of segment in bytes. */
-  pid_t              shm_lpid;	/* Process ID of last operation. */
-  pid_t              shm_cpid;	/* Process ID of creator. */
-  shmatt_t           shm_nattch;/* Number of current attaches. */
-  timestruc_t        shm_atim;	/* Time of last shmat (). */
-  timestruc_t        shm_dtim;	/* Time of last shmdt (). */
-  timestruc_t        shm_ctim;	/* Time of last change by shmctl (). */
-#ifdef _KERNEL
-  struct shm_handle *shm_internal;
-  long               shm_spare4[1];
+class _shmattach {
+public:
+  void *data;
+  int shmflg;
+  class _shmattach *next;
+};
+
+class shmid_ds {
+public:
+  struct   ipc_perm shm_perm;
+  size_t   shm_segsz;
+  pid_t    shm_lpid;
+  pid_t    shm_cpid;
+  shmatt_t shm_nattch;
+  time_t   shm_atime;
+  time_t   shm_dtime;
+  time_t   shm_ctime;
+  void *mapptr;
+};
+
+class shmnode {
+public:
+  class shmid_ds * shmds;
+  int shm_id;
+  class shmnode *next;
+  key_t key;
+  HANDLE filemap;
+  HANDLE attachmap;
+  class _shmattach *attachhead;
+};
+
 #else
-  long               shm_spare4[2];
-#endif /* _KERNEL */
-};
-
-#define shm_atime shm_atim.tv_sec
-#define shm_dtime shm_dtim.tv_sec
-#define shm_ctime shm_ctim.tv_sec
-
-#ifdef _KERNEL
-/* Buffer type for shmctl (IPC_INFO, ...) as used by ipcs(8).
+/* this is what we return when queried. It has no bitwise correspondence
+ * the internal structures 
  */
-struct shminfo
-{
-  long shmmax;		/* Maximum size in bytes of a shared
-			   memory segment. */
-  long shmmin;		/* Minimum size in bytes of a shared
-			   memory segment. */
-  long shmmni;		/* Maximum number of shared memory
-			   segments, system wide. */
-  long shmseg;		/* Maximum number of shared memory
-			   segments attached per process. */
-  long shmall;		/* Maximum number of bytes of shared
-			   memory, system wide. */
-  long shm_spare[4];
+struct shmid_ds {
+  struct   ipc_perm shm_perm;
+  size_t   shm_segsz;
+  pid_t    shm_lpid;
+  pid_t    shm_cpid;
+  shmatt_t shm_nattch;
+  time_t   shm_atime;
+  time_t   shm_dtime;
+  time_t   shm_ctime;
 };
+#endif /* __INSIDE_CYGWIN__ */
 
-/* Buffer type for shmctl (SHM_INFO, ...) as used by ipcs(8).
- */
-struct shm_info
-{
-#define shm_ids used_ids
-  long used_ids;	/* Number of allocated segments. */
-  long shm_tot;		/* Size in bytes of allocated segments. */
-  long shm_atts;	/* Number of attached segments, system
-			   wide. */
-};
-#endif /* _KERNEL */
+void *shmat(int, const void *, int);
+int   shmctl(int, int, struct shmid_ds *);
+int   shmdt(const void *);
+int   shmget(key_t, size_t, int);
 
-void *shmat (int shmid, const void *shmaddr, int shmflg);
-int   shmctl (int shmid, int cmd, struct shmid_ds *buf);
-int   shmdt (const void *shmaddr);
-int   shmget (key_t key, size_t size, int shmflg);
+#endif /* _SYS_SHM_H */
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* _CYGWIN_SHM_H */
