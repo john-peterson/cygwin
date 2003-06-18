4299e13a05ac2f0c8e36c44c988e4850164215fc
hp <hp>
2003-06-18 14:52:43 +0000

    * ld-cris/locref1.d, ld-cris/locref1.s, ld-cris/locref2.d, 	ld-cris/locref2.s: New tests.

--- ld/testsuite/ld-cris/locref1.d
+++ ld/testsuite/ld-cris/locref1.d
@@ -1,4 +1,4 @@
-#as: --no-underscore --pic --em=criself
+#as: --no-underscore --pic
 #source: expdyn1.s
 #source: locref1.s
 #ld: -m crislinux
@@ -11,12 +11,13 @@
 
 SYMBOL TABLE:
 #...
-0+82084 l     O \.got	0+ _GLOBAL_OFFSET_TABLE_
 0+80076 g     F \.text	0+2 expfn
-0+82090 g     O \.data	0+4 expobj
+0+820a0 g     O \.data	0+ expobj
+#...
+0+820a4 g     O \.got	0+ _GLOBAL_OFFSET_TABLE_
 #...
 Disassembly of section \.text:
 #...
 0+80078 <y>:
-   80078:	6fae f2df ffff  .*
-   8007e:	6fbe 0c00 0000  .*
+   80078:	6fae d2df ffff  .*
+   8007e:	6fbe fcff ffff  .*
--- ld/testsuite/ld-cris/locref2.d
+++ ld/testsuite/ld-cris/locref2.d
@@ -1,4 +1,4 @@
-#as: --no-underscore --pic --em=criself
+#as: --no-underscore --pic
 #source: gotrel1.s
 #source: locref2.s
 #ld: -m crislinux
@@ -11,12 +11,13 @@
 
 SYMBOL TABLE:
 #...
-0+82088 l     O \.got	0+ _GLOBAL_OFFSET_TABLE_
 0+  w      \*UND\*	0+ expfn
 0+  w      \*UND\*	0+ expobj
+#...
+0+820a0 g     O \.got	0+ _GLOBAL_OFFSET_TABLE_
 #...
 Disassembly of section \.text:
 #...
 0+8007c <y>:
-   8007c:	6fae 78df f7ff  .*
-   80082:	6fbe 78df f7ff  .*
+   8007c:	6fae 60df f7ff  .*
+   80082:	6fbe 60df f7ff  .*
