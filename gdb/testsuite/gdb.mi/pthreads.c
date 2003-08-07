7bb7606231e7b36ec6935646775baee32539d3c6
ezannoni <ezannoni>
2003-08-07 18:16:41 +0000

    2003-08-07  Elena Zannoni  <ezannoni@redhat.com>
           * gdb.base/completion.exp: Remove reduntant completion test
           on filename.
    
    2003-08-07  Elena Zannoni  <ezannoni@redhat.com>
    
            * gdb.base/attach.exp, gdb.base/detach.exp, gdb.base/gcore.exp,
            gdb.mi/mi-basics.exp, gdb.threads/gcore-thread.exp,
            gdb.trace/save-trace.exp: Make sure that full pathnames are
            escaped correctly.
    
    2003-08-07  Elena Zannoni  <ezannoni@redhat.com>
    
            * configure.in: Don't generate config.h from config.hin.
            * configure: Regenerate.
            * config.hin: Remove file.
            * gdb.mi/pthreads.c: Remove include of config.h, and other ifdefs.
            * gdb.threads/pthreads.c: Ditto.

--- gdb/testsuite/gdb.mi/pthreads.c
+++ gdb/testsuite/gdb.mi/pthreads.c
@@ -1,27 +1,4 @@
-/* Pthreads test program.
-   Copyright 1996-2013 Free Software Foundation, Inc.
-
-   Written by Keith Seitz of Red Hat.
-   Copied from gdb.threads/pthreads.c.
-   Contributed by Red Hat.
-
-   This file is part of GDB.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 
 /* Under OSF 2.0 & 3.0 and HPUX 10, the second arg of pthread_create
@@ -40,14 +17,7 @@ static pthread_attr_t null_attr;
 void *
 routine (void *arg)
 {
-  /* When gdb is running, it sets hidden breakpoints in the thread
-     library.  The signals caused by these hidden breakpoints can
-     cause system calls such as 'sleep' to return early.  Pay attention
-     to the return value from 'sleep' to get the full sleep.  */
-  int unslept = 9;
-  while (unslept > 0)
-    unslept = sleep (unslept);
-
+  sleep (9);
   printf ("hello thread\n");
 }
 
@@ -56,7 +26,7 @@ void
 done_making_threads (void)
 {
   /* Nothing */
-}
+};
 
 void
 create_thread (void)
