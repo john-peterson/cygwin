db2ccbdd50f7beb83142c2b198a40de13d55c848
ciceron <ciceron>
2003-08-08 20:45:31 +0000

    * sim-main.h (phys_to_virt): Use memory bank parameters to translate 	the physical address in virtual address. 	(struct _sim_cpu): Add memory bank members. 	* m68hc11_sim.c (cpu_initialize): Clear memory bank parameters. 	* interp.c (sim_hw_configure): Create memory bank according to memory 	bank parameters. 	(sim_get_bank_parameters): New function to obtain memory bank config 	from the symbol table. 	(sim_prepare_for_program): Call it to obtain the memory bank parameters. 	(sim_open): Call sim_prepare_for_program. 	* dv-m68hc11.c (m68hc11cpu_io_write_buffer): Use memory bank parameters 	to check if address is within bank window. 	(m68hc11cpu_io_read_buffer): Likewise. 	(attach_m68hc11_regs): Map the memory bank according to memory bank 	parameters.

--- sim/m68hc11/dv-m68hc11.c
+++ sim/m68hc11/dv-m68hc11.c
@@ -1,20 +1,21 @@
 /*  dv-m68hc11.c -- CPU 68HC11&68HC12 as a device.
-    Copyright (C) 1999-2013 Free Software Foundation, Inc.
+    Copyright (C) 1999, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
     Written by Stephane Carrez (stcarrez@nerim.fr)
     (From a driver model Contributed by Cygnus Solutions.)
     
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
+    the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-
+    
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
+    
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
     
     */
 
@@ -807,15 +808,13 @@ m68hc11_option_handler (SIM_DESC sd, sim_cpu *cpu,
                                "      %d       %d    %35.35s\n",
                                osc->name, freq,
                                cur_value, next_value,
-                               cycle_to_string (cpu, t,
-                                                PRINT_TIME | PRINT_CYCLE));
+                               cycle_to_string (cpu, t));
               else
                 sim_io_printf (sd, " %4.4s  %8.8s hz "
                                "      %d       %d    %35.35s\n",
                                osc->name, freq,
                                cur_value, next_value,
-                               cycle_to_string (cpu, t,
-                                                PRINT_TIME | PRINT_CYCLE));
+                               cycle_to_string (cpu, t));
             }
         }
       break;      
