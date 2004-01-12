a0727d8b6de53c6379d292b1c11347ec3d3939b2
cgf <cgf>
2004-01-12 05:15:18 +0000

    * Makefile.in (DLL_IMPORTS): Link advapi32 to ensure proper DLL initialization. * autoload.cc (RegCloseKey): Arbitrarily choose this function as a "seed" to pull the advapi32 link library in.  So, comment out the autoloading. * cygtls.cc (_threadinfo::init_thread): Just clear CYGTLS_PADSIZE. (_threadinfo::remove): Add debugging. (_threadinfo::find_tls): Ditto. * cygtls.h (_threadinfo::padding): Make zero length (for now?). * dcrt0.cc (dll_crt0_0): Move more initialization here from dll_crt0_1. (dll_crt0_1): See above. * dtable.h (dtable::lock): Remove commented out critical section locking. * dtable.h (dtable::init_lock): Remove commented out critical section locking. * dtable.h (dtable::unlock): Remove commented out critical section locking. * exceptions.cc (interruptible): bool'ize. * init.cc (threadfunc_fe): Revert to storing threadfunc at stack bottom. (munge_threadfunc): Ditto.  Avoid adding overhead to calibration_thread. (prime_threads): Don't initialize tls stuff. (dll_entry): Make minor change to initialization order. * tlsoffsets.h: Regenerate. * sigproc.cc (wait_sig): Add sanity check for end of process thread exit. * select.h: Make minor formatting change.

--- winsup/cygwin/select.h
+++ winsup/cygwin/select.h
@@ -1,7 +1,6 @@
 /* select.h
 
-   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
-   2007, 2008, 2009, 2010, 2011, 2012 Red Hat, Inc.
+   Copyright 1998, 1999, 2000, 2001, 2004 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -9,102 +8,48 @@ This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
 details. */
 
-#ifndef _SELECT_H_
-#define _SELECT_H_
+/* Winsock select() types and macros */
 
-struct select_record
+/*
+ * Use this struct to interface to
+ * the system provided select.
+ */
+typedef struct winsock_fd_set
 {
-  int fd;
-  HANDLE h;
-  fhandler_base *fh;
-  int thread_errno;
-  bool windows_handle;
-  bool read_ready, write_ready, except_ready;
-  bool read_selected, write_selected, except_selected;
-  bool except_on_write;
-  int (*startup) (select_record *, class select_stuff *);
-  int (*peek) (select_record *, bool);
-  int (*verify) (select_record *, fd_set *, fd_set *, fd_set *);
-  void (*cleanup) (select_record *, class select_stuff *);
-  struct select_record *next;
-  void set_select_errno () {__seterrno (); thread_errno = errno;}
-  int saw_error () {return thread_errno;}
-  select_record (int): next (NULL) {}
-  select_record (): fd (0), h (NULL), fh (NULL), thread_errno (0),
-    windows_handle (false), read_ready (false), write_ready (false),
-    except_ready (false), read_selected (false), write_selected (false),
-    except_selected (false), except_on_write (false),
-    startup (NULL), peek (NULL), verify (NULL), cleanup (NULL),
-    next (NULL) {}
-#ifdef DEBUGGING
-  void dump_select_record ();
-#endif
-};
+  unsigned int fd_count;
+  HANDLE fd_array[1024]; /* Dynamically allocated. */
+} winsock_fd_set;
 
-struct select_info
-{
-  cygthread *thread;
-  bool stop_thread;
-  select_record *start;
-  select_info (): thread (NULL), stop_thread (0), start (NULL) {}
-};
+/*
+ * Define the Win32 winsock definitions to have a prefix WINSOCK_
+ * so we can be explicit when we are using them.
+ */
+#define WINSOCK_FD_ISSET(fd, set) __WSAFDIsSet ((SOCKET)fd, (fd_set *)set)
+#define WINSOCK_FD_SET(fd, set) do { \
+	       (set)->fd_array[(set)->fd_count++]=fd;\
+} while(0)
+#define WINSOCK_FD_ZERO(set) ((set)->fd_count = 0)
+#define WINSOCK_FD_CLR(fd, set) do { \
+    u_int __i; \
+    for (__i = 0; __i < (set)->fd_count ; __i++) { \
+	if ((set)->fd_array[__i] == fd) { \
+	    while (__i < (set)->fd_count-1) { \
+		(set)->fd_array[__i] = \
+		    (set)->fd_array[__i+1]; \
+		__i++; \
+	    } \
+	    (set)->fd_count--; \
+	    break; \
+	} \
+    } \
+} while(0)
 
-struct select_pipe_info: public select_info
-{
-  select_pipe_info (): select_info () {}
-};
+extern "C" int PASCAL __WSAFDIsSet(SOCKET, fd_set*);
+extern "C" int PASCAL win32_select(int, fd_set*, fd_set*, fd_set*, const struct timeval*);
 
-struct select_socket_info: public select_info
-{
-  int num_w4;
-  LONG *ser_num;
-  HANDLE *w4;
-  select_socket_info (): select_info (), num_w4 (0), ser_num (0), w4 (NULL) {} 
-};
-
-struct select_serial_info: public select_info
-{
-  select_serial_info (): select_info () {}
-};
-
-struct select_mailslot_info: public select_info
-{
-  select_mailslot_info (): select_info () {}
-};
-
-class select_stuff
-{
-public:
-  enum wait_states
-  {
-    select_signalled = -3,
-    select_loop = -2,
-    select_error = -1,
-    select_ok = 0,
-    select_set_zero = 1
-  };
-
-  ~select_stuff ();
-  bool return_on_signal;
-  bool always_ready, windows_used;
-  select_record start;
-
-  select_pipe_info *device_specific_pipe;
-  select_socket_info *device_specific_socket;
-  select_serial_info *device_specific_serial;
-  select_mailslot_info *device_specific_mailslot;
-
-  bool test_and_set (int, fd_set *, fd_set *, fd_set *);
-  int poll (fd_set *, fd_set *, fd_set *);
-  wait_states wait (fd_set *, fd_set *, fd_set *, DWORD);
-  void cleanup ();
-  void destroy ();
-
-  select_stuff (): return_on_signal (false), always_ready (false),
-  		   windows_used (false), start (0),
-		   device_specific_pipe (NULL),
-		   device_specific_socket (NULL),
-		   device_specific_serial (NULL),
-		   device_specific_mailslot (NULL) {}
-};
-#endif /* _SELECT_H_ */
+/*
+ * call to winsock's select() -
+ * type coercion need to appease confused prototypes
+ */
+#define WINSOCK_SELECT(nfd, rd, wr, ex, timeo) \
+  win32_select (nfd, (fd_set *) rd, (fd_set *) wr, (fd_set *) ex, timeo)
