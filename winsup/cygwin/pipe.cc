e6c87d8962c4ec6f14c527c45824c11ea9479bb6
cgf <cgf>
2004-01-17 04:13:19 +0000

    * cygtls.h (_threadinfo::threadkill): New element. (_threadinfo::set_threadkill): Declare new function. (_threadinfo::reset_threadkill): Declare new function. * dcrt0.cc (dcrt0_1): Call here so that it will be possible to attach to running process with #(*& Windows Me/9x. (initial_env): Try to initialize strace if uninitialized. * gendef: Don't zero signal if threadkill is set since that will happen in the called function. * signal.cc (sigwait): Ensure cleanup in error conditions. * sigproc.cc (sig_send): Clear packet mask storage. (wait_subproc): Fill in child exit code in siginfo_t structure. * thread.cc (pthread_kill): Set threadkill flag. * tlsoffsets.h: Regenerate.  Throughout, use siginfo_t to fill out all signal information for "kernel" signals. * cygtls.h (_threadinfo::set_siginfo): Declare new function. * cygtls.cc (_threadinfo::set_siginfo): Define new function. * dcrt0.cc (do_exit): Accommodate siginfo_t considerations. * exceptions.cc (handle_exceptions): Ditto. (sig_handle_tty_stop): Ditto. (ctrl_c_handler): Use killsys() to send signal. (sigpacket::process): Rename from sig_handle.  Use siginfo_t field from sigpacket for everything. (tty_min::kill_pgrp): Accommodate siginfo_t considerations. (fhandler_termios::bg_check): Ditto. * fhandler_tty.cc (fhandler_tty_slave::ioctl): Use killsys() to send signal. * signal.cc (kill_worker): Rewrite to use siginfo_t second argument. (kill_pgrp): Ditto. (kill0): Define new function pulled from kill(). (kill): Rewrite as frontend to kill0. (killsys): Define new function. * sigproc.cc (sigelem): Eliminate. (sigpacket): Move to sigproc.h.  Subsume sigelem. (pending_signals): Use sigpacket rather than sigelem for everything. (sig_clear): Ditto. (wait_sig): Ditto. (sig_send): Rewrite to use siginfo_t argument. (sig_send): New function wratpper to sig_send with siginfo_t argument. (wait_subproc): Accommodate siginfo_t considerations. * thread.cc (pthread_kill): Ditto. * sigproc.h (sigpacket): Move here. (sigpacket::process): Declare "new" function. (sig_handle): Eliminate declaration. (sig_send): Declare with new paramaters. (killsys): Declare new function. (kill_pgrp): Declare. * winsup.h: Move some signal-specific stuff to sigproc.h. * include/cygwin/signal.h: Tweak some siginfo_t stuff. * fhandler_console.cc (fhandler_console::close): Remove obsolete test for vfork_cleanup. * pipe.cc (fhandler_pipe::close): Add comment. * cygheap.cc (init_cygheap::close_ctty): Don't NULL ctty if it is still active. * dtable.cc (dtable::vfork_parent_restore): Store ctty_on_hold prior to calling close_all_files since it will be zeroed.

--- winsup/cygwin/pipe.cc
+++ winsup/cygwin/pipe.cc
@@ -1,7 +1,6 @@
 /* pipe.cc: pipe for Cygwin.
 
-   Copyright 1996, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007,
-   2008, 2009, 2010, 2011, 2012 Hat, Inc.
+   Copyright 1996, 1998, 1999, 2000, 2001, 2002, 2003 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -12,7 +11,7 @@ details. */
 /* FIXME: Should this really be fhandler_pipe.cc? */
 
 #include "winsup.h"
-#include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include "cygerrno.h"
 #include "security.h"
@@ -20,126 +19,17 @@ details. */
 #include "fhandler.h"
 #include "dtable.h"
 #include "cygheap.h"
+#include "thread.h"
 #include "pinfo.h"
-#include "shared_info.h"
+#include "cygthread.h"
+
+static unsigned pipecount;
+static const NO_COPY char pipeid_fmt[] = "stupid_pipe.%u.%u";
 
 fhandler_pipe::fhandler_pipe ()
-  : fhandler_base_overlapped (), popen_pid (0)
+  : fhandler_base (), guard (NULL), broken_pipe (false), writepipe_exists(0),
+    orig_pid (0), id (0)
 {
-  max_atomic_write = DEFAULT_PIPEBUFSIZE;
-  need_fork_fixup (true);
-}
-
-int
-fhandler_pipe::init (HANDLE f, DWORD a, mode_t mode)
-{
-  /* FIXME: Have to clean this up someday
-     FIXME: Do we have to check for both !get_win32_name() and
-     !*get_win32_name()? */
-  if ((!get_win32_name () || !*get_win32_name ()) && get_name ())
-    {
-      char *d;
-      const char *s;
-      char *hold_normalized_name = (char *) alloca (strlen (get_name ()) + 1);
-      for (s = get_name (), d = hold_normalized_name; *s; s++, d++)
-	if (*s == '/')
-	  *d = '\\';
-	else
-	  *d = *s;
-      *d = '\0';
-      set_name (hold_normalized_name);
-    }
-
-  bool opened_properly = a & FILE_CREATE_PIPE_INSTANCE;
-  a &= ~FILE_CREATE_PIPE_INSTANCE;
-  fhandler_base::init (f, a, mode);
-  close_on_exec (mode & O_CLOEXEC);
-  if (opened_properly)
-    setup_overlapped ();
-  else
-    destroy_overlapped ();
-  return 1;
-}
-
-extern "C" int sscanf (const char *, const char *, ...);
-
-int
-fhandler_pipe::open (int flags, mode_t mode)
-{
-  HANDLE proc, pipe_hdl, nio_hdl = NULL;
-  fhandler_pipe *fh = NULL;
-  size_t size;
-  int pid, rwflags = (flags & O_ACCMODE);
-  bool inh;
-
-  sscanf (get_name (), "/proc/%d/fd/pipe:[%d]", &pid, (int *) &pipe_hdl);
-  if (pid == myself->pid)
-    {
-      cygheap_fdenum cfd (true);
-      while (cfd.next () >= 0)
-	{
-	  if (cfd->get_handle () != pipe_hdl)
-	    continue;
-	  if ((rwflags == O_RDONLY && !(cfd->get_access () & GENERIC_READ))
-	      || (rwflags == O_WRONLY && !(cfd->get_access () & GENERIC_WRITE)))
-	    {
-	      set_errno (EACCES);
-	      return 0;
-	    }
-	  cfd->copyto (this);
-	  set_io_handle (NULL);
-	  pc.reset_conv_handle ();
-	  if (!cfd->dup (this, flags))
-	    return 1;
-	  return 0;
-	}
-      set_errno (ENOENT);
-      return 0;
-    }
-
-  pinfo p (pid);
-  if (!p)
-    {
-      set_errno (ESRCH);
-      return 0;
-    }
-  if (!(proc = OpenProcess (PROCESS_DUP_HANDLE, false, p->dwProcessId)))
-    {
-      __seterrno ();
-      return 0;
-    }
-  if (!(fh = p->pipe_fhandler (pipe_hdl, size)) || !size)
-    {
-      set_errno (ENOENT);
-      goto out;
-    }
-  /* Too bad, but Windows only allows the same access mode when dup'ing
-     the pipe. */
-  if ((rwflags == O_RDONLY && !(fh->get_access () & GENERIC_READ))
-      || (rwflags == O_WRONLY && !(fh->get_access () & GENERIC_WRITE)))
-    {
-      set_errno (EACCES);
-      goto out;
-    }
-  inh = !(flags & O_CLOEXEC);
-  if (!DuplicateHandle (proc, pipe_hdl, GetCurrentProcess (), &nio_hdl,
-			0, inh, DUPLICATE_SAME_ACCESS))
-    {
-      __seterrno ();
-      goto out;
-    }
-  init (nio_hdl, fh->get_access (), mode & O_TEXT ?: O_BINARY);
-  cfree (fh);
-  CloseHandle (proc);
-  return 1;
-out:
-  if (nio_hdl)
-    CloseHandle (nio_hdl);
-  if (fh)
-    free (fh);
-  if (proc)
-    CloseHandle (proc);
-  return 0;
 }
 
 _off64_t
@@ -150,218 +40,189 @@ fhandler_pipe::lseek (_off64_t offset, int whence)
   return -1;
 }
 
-int
-fhandler_pipe::fadvise (_off64_t offset, _off64_t length, int advice)
+void
+fhandler_pipe::set_close_on_exec (int val)
 {
-  set_errno (ESPIPE);
-  return -1;
+  fhandler_base::set_close_on_exec (val);
+  if (guard)
+    set_inheritance (guard, val);
+  if (writepipe_exists)
+    set_inheritance (writepipe_exists, val);
+}
+
+struct pipeargs
+{
+  fhandler_base *fh;
+  void *ptr;
+  size_t *len;
+};
+
+static DWORD WINAPI
+read_pipe (void *arg)
+{
+  pipeargs *pi = (pipeargs *) arg;
+  pi->fh->fhandler_base::read (pi->ptr, *pi->len);
+  return 0;
+}
+
+void __stdcall
+fhandler_pipe::read (void *in_ptr, size_t& in_len)
+{
+  if (broken_pipe)
+    in_len = 0;
+  else
+    {
+      pipeargs pi = {dynamic_cast<fhandler_base *>(this), in_ptr, &in_len};
+      ResetEvent (read_state);
+      cygthread *th = new cygthread (read_pipe, &pi, "read_pipe");
+      if (th->detach (read_state) && !in_len)
+	in_len = (size_t) -1;	/* received a signal */
+    }
+  (void) ReleaseMutex (guard);
+  return;
 }
 
 int
-fhandler_pipe::ftruncate (_off64_t length, bool allow_truncate)
+fhandler_pipe::close ()
 {
-  set_errno (allow_truncate ? EINVAL : ESPIPE);
-  return -1;
+  if (guard)
+    CloseHandle (guard);
+  if (writepipe_exists)
+    CloseHandle (writepipe_exists);
+  // FIXME is this vfork_cleanup test right?  Is it responsible for some of
+  // the strange pipe behavior that has been reported in the cygwin mailing
+  // list?
+  if (read_state && !cygheap->fdtab.in_vfork_cleanup ())
+    CloseHandle (read_state);
+  if (get_handle ())
+    {
+      CloseHandle (get_handle ());
+      set_io_handle (NULL);
+    }
+  return 0;
 }
 
-char *
-fhandler_pipe::get_proc_fd_name (char *buf)
+bool
+fhandler_pipe::hit_eof ()
 {
-  __small_sprintf (buf, "pipe:[%d]", get_handle ());
-  return buf;
+  char buf[80];
+  HANDLE ev;
+  if (broken_pipe)
+    return 1;
+  if (!orig_pid)
+    return false;
+  __small_sprintf (buf, pipeid_fmt, orig_pid, id);
+  if ((ev = OpenEvent (EVENT_ALL_ACCESS, FALSE, buf)))
+    CloseHandle (ev);
+  debug_printf ("%s %p", buf, ev);
+  return ev == NULL;
+}
+
+void
+fhandler_pipe::fixup_after_exec (HANDLE parent)
+{
+  if (read_state)
+    read_state = CreateEvent (&sec_none_nih, FALSE, FALSE, NULL);
+}
+
+void
+fhandler_pipe::fixup_after_fork (HANDLE parent)
+{
+  fhandler_base::fixup_after_fork (parent);
+  if (guard)
+    fork_fixup (parent, guard, "guard");
+  if (writepipe_exists)
+    fork_fixup (parent, writepipe_exists, "guard");
+  fixup_after_exec (parent);
 }
 
 int
-fhandler_pipe::dup (fhandler_base *child, int flags)
+fhandler_pipe::dup (fhandler_base *child)
 {
+  if (get_handle ())
+    {
+      int res = fhandler_base::dup (child);
+      if (res)
+	return res;
+    }
+
   fhandler_pipe *ftp = (fhandler_pipe *) child;
-  ftp->set_popen_pid (0);
 
-  int res;
-  if (get_handle () && fhandler_base_overlapped::dup (child, flags))
-    res = -1;
-  else
-    res = 0;
-
-  debug_printf ("res %d", res);
-  return res;
-}
-
-#define PIPE_INTRO "\\\\.\\pipe\\cygwin-"
-
-/* Create a pipe, and return handles to the read and write ends,
-   just like CreatePipe, but ensure that the write end permits
-   FILE_READ_ATTRIBUTES access, on later versions of win32 where
-   this is supported.  This access is needed by NtQueryInformationFile,
-   which is used to implement select and nonblocking writes.
-   Note that the return value is either 0 or GetLastError,
-   unlike CreatePipe, which returns a bool for success or failure.  */
-DWORD
-fhandler_pipe::create (LPSECURITY_ATTRIBUTES sa_ptr, PHANDLE r, PHANDLE w,
-		       DWORD psize, const char *name, DWORD open_mode)
-{
-  /* Default to error. */
-  if (r)
-    *r = NULL;
-  if (w)
-    *w = NULL;
-
-  /* Ensure that there is enough pipe buffer space for atomic writes.  */
-  if (!psize)
-    psize = DEFAULT_PIPEBUFSIZE;
-
-  char pipename[MAX_PATH];
-  size_t len = __small_sprintf (pipename, PIPE_INTRO "%S-",
-				      &cygheap->installation_key);
-  DWORD pipe_mode = PIPE_READMODE_BYTE;
-  if (!name)
-    pipe_mode |= pipe_byte ? PIPE_TYPE_BYTE : PIPE_TYPE_MESSAGE;
-  else
-    pipe_mode |= PIPE_TYPE_MESSAGE;
-
-  if (!name || (open_mode & PIPE_ADD_PID))
+  /* FIXME: This leaks handles in the failing condition */
+  if (guard == NULL)
+    ftp->guard = NULL;
+  else if (!DuplicateHandle (hMainProc, guard, hMainProc, &ftp->guard, 0, 1,
+			     DUPLICATE_SAME_ACCESS))
     {
-      len += __small_sprintf (pipename + len, "%u-", GetCurrentProcessId ());
-      open_mode &= ~PIPE_ADD_PID;
+      debug_printf ("couldn't duplicate guard %p, %E", guard);
+      return -1;
     }
 
-  if (name)
-    len += __small_sprintf (pipename + len, "%s", name);
-
-  open_mode |= PIPE_ACCESS_INBOUND;
-
-  /* Retry CreateNamedPipe as long as the pipe name is in use.
-     Retrying will probably never be necessary, but we want
-     to be as robust as possible.  */
-  DWORD err = 0;
-  while (r && !*r)
+  if (writepipe_exists == NULL)
+    ftp->writepipe_exists = NULL;
+  else if (!DuplicateHandle (hMainProc, writepipe_exists, hMainProc,
+			     &ftp->writepipe_exists, 0, 1,
+			     DUPLICATE_SAME_ACCESS))
     {
-      static volatile ULONG pipe_unique_id;
-      if (!name)
-	__small_sprintf (pipename + len, "pipe-%p",
-			 InterlockedIncrement ((LONG *) &pipe_unique_id));
-
-      debug_printf ("name %s, size %lu, mode %s", pipename, psize,
-		    (pipe_mode & PIPE_TYPE_MESSAGE)
-		    ? "PIPE_TYPE_MESSAGE" : "PIPE_TYPE_BYTE");
-
-      /* Use CreateNamedPipe instead of CreatePipe, because the latter
-	 returns a write handle that does not permit FILE_READ_ATTRIBUTES
-	 access, on versions of win32 earlier than WinXP SP2.
-	 CreatePipe also stupidly creates a full duplex pipe, which is
-	 a waste, since only a single direction is actually used.
-	 It's important to only allow a single instance, to ensure that
-	 the pipe was not created earlier by some other process, even if
-	 the pid has been reused.  We avoid FILE_FLAG_FIRST_PIPE_INSTANCE
-	 because that is only available for Win2k SP2 and WinXP.
-
-	 Note that the write side of the pipe is opened as PIPE_TYPE_MESSAGE.
-	 This *seems* to more closely mimic Linux pipe behavior and is
-	 definitely required for pty handling since fhandler_pty_master
-	 writes to the pipe in chunks, terminated by newline when CANON mode
-	 is specified.  */
-      *r = CreateNamedPipe (pipename, open_mode, pipe_mode, 1, psize,
-			   psize, NMPWAIT_USE_DEFAULT_WAIT, sa_ptr);
-
-      if (*r != INVALID_HANDLE_VALUE)
-	{
-	  debug_printf ("pipe read handle %p", *r);
-	  err = 0;
-	  break;
-	}
-
-      err = GetLastError ();
-      switch (err)
-	{
-	case ERROR_PIPE_BUSY:
-	  /* The pipe is already open with compatible parameters.
-	     Pick a new name and retry.  */
-	  debug_printf ("pipe busy", !name ? ", retrying" : "");
-	  if (!name)
-	    *r = NULL;
-	  break;
-	case ERROR_ACCESS_DENIED:
-	  /* The pipe is already open with incompatible parameters.
-	     Pick a new name and retry.  */
-	  debug_printf ("pipe access denied%s", !name ? ", retrying" : "");
-	  if (!name)
-	    *r = NULL;
-	  break;
-	default:
-	  {
-	    err = GetLastError ();
-	    debug_printf ("failed, %E");
-	  }
-	}
+      debug_printf ("couldn't duplicate writepipe_exists %p, %E", writepipe_exists);
+      return -1;
     }
 
-  if (err)
+  if (read_state == NULL)
+    ftp->read_state = NULL;
+  else if (!DuplicateHandle (hMainProc, read_state, hMainProc,
+			     &ftp->read_state, 0, 1,
+			     DUPLICATE_SAME_ACCESS))
     {
-      *r = NULL;
-      return err;
+      debug_printf ("couldn't duplicate read_state %p, %E", writepipe_exists);
+      return -1;
     }
 
-  if (!w)
-    debug_printf ("pipe write handle NULL");
-  else
-    {
-      debug_printf ("CreateFile: name %s", pipename);
-
-      /* Open the named pipe for writing.
-	 Be sure to permit FILE_READ_ATTRIBUTES access.  */
-      DWORD access = GENERIC_WRITE | FILE_READ_ATTRIBUTES;
-      if ((open_mode & PIPE_ACCESS_DUPLEX) == PIPE_ACCESS_DUPLEX)
-	access |= GENERIC_READ | FILE_WRITE_ATTRIBUTES;
-      *w = CreateFile (pipename, access, 0, sa_ptr, OPEN_EXISTING,
-		      open_mode & FILE_FLAG_OVERLAPPED, 0);
-
-      if (!*w || *w == INVALID_HANDLE_VALUE)
-	{
-	  /* Failure. */
-	  DWORD err = GetLastError ();
-	  debug_printf ("CreateFile failed, r %p, %E", r);
-	  if (r)
-	    CloseHandle (*r);
-	  *w = NULL;
-	  return err;
-	}
-
-      debug_printf ("pipe write handle %p", *w);
-    }
-
-  /* Success. */
+  ftp->id = id;
+  ftp->orig_pid = orig_pid;
   return 0;
 }
 
 int
-fhandler_pipe::create (fhandler_pipe *fhs[2], unsigned psize, int mode)
+fhandler_pipe::create (fhandler_pipe *fhs[2], unsigned psize, int mode, bool fifo)
 {
   HANDLE r, w;
-  SECURITY_ATTRIBUTES *sa = sec_none_cloexec (mode);
+  SECURITY_ATTRIBUTES *sa = (mode & O_NOINHERIT) ?  &sec_none_nih : &sec_none;
   int res = -1;
 
-  int ret = create (sa, &r, &w, psize, NULL, FILE_FLAG_OVERLAPPED);
-  if (ret)
-    __seterrno_from_win_error (ret);
-  else if ((fhs[0] = (fhandler_pipe *) build_fh_dev (*piper_dev)) == NULL)
-    {
-      CloseHandle (r);
-      CloseHandle (w);
-    }
-  else if ((fhs[1] = (fhandler_pipe *) build_fh_dev (*pipew_dev)) == NULL)
-    {
-      delete fhs[0];
-      CloseHandle (w);
-    }
+  if (!CreatePipe (&r, &w, sa, psize))
+    __seterrno ();
   else
     {
-      mode |= mode & O_TEXT ?: O_BINARY;
-      fhs[0]->init (r, FILE_CREATE_PIPE_INSTANCE | GENERIC_READ, mode);
-      fhs[1]->init (w, FILE_CREATE_PIPE_INSTANCE | GENERIC_WRITE, mode);
+      fhs[0] = (fhandler_pipe *) build_fh_dev (*piper_dev);
+      fhs[1] = (fhandler_pipe *) build_fh_dev (*pipew_dev);
+
+      int binmode = mode & O_TEXT ?: O_BINARY;
+      fhs[0]->init (r, GENERIC_READ, binmode);
+      fhs[1]->init (w, GENERIC_WRITE, binmode);
+      if (mode & O_NOINHERIT)
+       {
+	 fhs[0]->set_close_on_exec_flag (1);
+	 fhs[1]->set_close_on_exec_flag (1);
+       }
+
+      fhs[0]->read_state = CreateEvent (&sec_none_nih, FALSE, FALSE, NULL);
+      fhs[0]->set_need_fork_fixup ();
+
       res = 0;
+      fhs[0]->create_guard (sa);
+      if (wincap.has_unreliable_pipes ())
+	{
+	  char buf[80];
+	  int count = pipecount++;	/* FIXME: Should this be InterlockedIncrement? */
+	  __small_sprintf (buf, pipeid_fmt, myself->pid, count);
+	  fhs[1]->writepipe_exists = CreateEvent (sa, TRUE, FALSE, buf);
+	  fhs[0]->orig_pid = myself->pid;
+	  fhs[0]->id = count;
+	}
     }
 
-  debug_printf ("%R = pipe([%p, %p], %d, %p)", res, fhs[0], fhs[1], psize, mode);
+  syscall_printf ("%d = ([%p, %p], %d, %p)", res, fhs[0], fhs[1], psize, mode);
   return res;
 }
 
@@ -392,79 +253,42 @@ fhandler_pipe::ioctl (unsigned int cmd, void *p)
   return 0;
 }
 
-int __stdcall
-fhandler_pipe::fstatvfs (struct statvfs *sfs)
+extern "C" int
+pipe (int filedes[2])
 {
-  set_errno (EBADF);
-  return -1;
-}
-
-static int __reg3
-pipe_worker (int filedes[2], unsigned int psize, int mode)
-{
+  extern DWORD binmode;
   fhandler_pipe *fhs[2];
-  int res = fhandler_pipe::create (fhs, psize, mode);
-  if (!res)
+  int res = fhandler_pipe::create (fhs, 16384, (!binmode || binmode == O_BINARY)
+					       ? O_BINARY : O_TEXT);
+  if (res == 0)
     {
       cygheap_fdnew fdin;
       cygheap_fdnew fdout (fdin, false);
-      char buf[sizeof ("/dev/fd/pipe:[2147483647]")];
-      __small_sprintf (buf, "/dev/fd/pipe:[%d]", (int) fdin);
-      fhs[0]->pc.set_normalized_path (buf);
-      __small_sprintf (buf, "pipe:[%d]", (int) fdout);
-      fhs[1]->pc.set_normalized_path (buf);
       fdin = fhs[0];
       fdout = fhs[1];
       filedes[0] = fdin;
       filedes[1] = fdout;
     }
+
   return res;
 }
 
 extern "C" int
 _pipe (int filedes[2], unsigned int psize, int mode)
 {
-  int res = pipe_worker (filedes, psize, mode);
-  int read, write;
-  if (res != 0)
-    read = write = -1;
-  else
+  fhandler_pipe *fhs[2];
+  int res = fhandler_pipe::create (fhs, psize, mode);
+  /* This type of pipe is not interruptible so set the appropriate flag. */
+  if (!res)
     {
-      read = filedes[0];
-      write = filedes[1];
+      cygheap_fdnew fdin;
+      cygheap_fdnew fdout (fdin, false);
+      fhs[0]->set_r_no_interrupt (1);
+      fdin = fhs[0];
+      fdout = fhs[1];
+      filedes[0] = fdin;
+      filedes[1] = fdout;
     }
-  syscall_printf ("%R = _pipe([%d, %d], %u, %p)", res, read, write, psize, mode);
-  return res;
-}
 
-extern "C" int
-pipe (int filedes[2])
-{
-  int res = pipe_worker (filedes, DEFAULT_PIPEBUFSIZE, O_BINARY);
-  int read, write;
-  if (res != 0)
-    read = write = -1;
-  else
-    {
-      read = filedes[0];
-      write = filedes[1];
-    }
-  syscall_printf ("%R = pipe([%d, %d])", res, read, write);
-  return res;
-}
-
-extern "C" int
-pipe2 (int filedes[2], int mode)
-{
-  int res = pipe_worker (filedes, DEFAULT_PIPEBUFSIZE, mode);
-  int read, write;
-  if (res != 0)
-    read = write = -1;
-  else
-    {
-      read = filedes[0];
-      write = filedes[1];
-    }
-  syscall_printf ("%R = pipe2([%d, %d], %p)", res, read, write, mode);
   return res;
 }
