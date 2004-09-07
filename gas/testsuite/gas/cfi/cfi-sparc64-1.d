0e2b044e1760959518d810306234c2c9cbb8fa79
ebotcazou <ebotcazou>
2004-09-07 07:12:15 +0000

    Merge from mainline: 	2004-04-19  Jakub Jelinek  <jakub@redhat.com> 	* gas/cfi/cfi-sparc64-1.d: Update.

--- gas/testsuite/gas/cfi/cfi-sparc64-1.d
+++ gas/testsuite/gas/cfi/cfi-sparc64-1.d
@@ -2,7 +2,7 @@
 #name: CFI on SPARC 64-bit
 #as: -64
 
-Contents of the .eh_frame section:
+The section .eh_frame contains:
 
 00000000 00000014 00000000 CIE
   Version:               1
@@ -17,9 +17,9 @@ Contents of the .eh_frame section:
   DW_CFA_nop
   DW_CFA_nop
 
-00000018 00000014 0000001c FDE cie=00000000 pc=00000000..00000030
-  DW_CFA_advance_loc: 4 to 00000004
-  DW_CFA_def_cfa_register: r30
+00000018 00000014 0000001c FDE cie=00000000 pc=00000020..00000050
+  DW_CFA_advance_loc: 4 to 00000024
+  DW_CFA_def_cfa_reg: r30
   DW_CFA_GNU_window_save
   DW_CFA_register: r15 in r31
 
