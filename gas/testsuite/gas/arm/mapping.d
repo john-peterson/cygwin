ec7004191ff7562e62a7582735f49d40c5ed4331
jules <jules>
2005-03-30 17:28:18 +0000

    * bfd/bfd-in.h (bfd_is_arm_mapping_symbol_name): Add prototype. 	* bfd/bfd-in2.h: Regenerate. 	* bfd/elf32-arm.c (elf32_arm_is_target_special_symbol): Rename call to 	bfd_is_arm_mapping_symbol_name. 	(elf32_arm_output_symbol_hook): Likewise. 	(arm_elf_find_function): Likewise, and include STT_NOTYPE in test for 	mapping symbols. 	(is_arm_mapping_symbol_name): Function moved from here... 	* bfd/cpu-arm.c (bfd_is_arm_mapping_symbol_name): ...to here, 	renamed and made global. 	* gas/config/tc-arm.c (mapping_state): Change documentation in 	function comment to cross-reference spec instead. Change type of 	mapping symbols to BSF_NO_TYPE. 	(arm_adjust_symtab): Don't change type of mapping symbols here. 	* gas/testsuite/gas/arm/mapping.d: Update expected output. 	* ld/testsuite/ld-arm/arm-app-abs32.d: Likewise. 	* ld/testsuite/ld-arm/arm-app.d: Likewise. 	* ld/testsuite/ld-arm/mixed-app.d: Likewise.

--- gas/testsuite/gas/arm/mapping.d
+++ gas/testsuite/gas/arm/mapping.d
@@ -1,7 +1,5 @@
 #objdump: --syms --special-syms
 #name: ARM Mapping Symbols
-# This test is only valid on ELF based ports.
-#not-target: *-*-*coff *-*-pe *-*-wince *-*-*aout* *-*-netbsd *-*-riscix*
 
 # Test the generation of ARM ELF Mapping Symbols
 
@@ -13,9 +11,8 @@ SYMBOL TABLE:
 0+00 l    d  .bss	0+0 (|.bss)
 0+00 l       .text	0+0 \$a
 0+08 l       .text	0+0 \$t
+0+00 l       .data	0+0 \$d
 0+00 l    d  foo	0+0 (|foo)
 0+00 l       foo	0+0 \$t
-#Maybe section symbol for .ARM.attributes
-#...
 0+00 g       .text	0+0 mapping
 0+08 g     F .text	0+0 thumb_mapping
