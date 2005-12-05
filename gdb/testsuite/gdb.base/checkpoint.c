d4270450eea40b68c6598f718afce420ea73d52b
msnyder <msnyder>
2005-12-05 20:15:29 +0000

    2005-12-05  Michael Snyder  <msnyder@redhat.com>
    	* gdb.base/checkpoint.c: New file.
    	* gdb.base/checkpoint.exp: New file.  Test checkpoint/restart
    	on Linux (using fork).
    	* gdb.base/pi.txt: New file, test load for checkpoint.exp test.

--- gdb/testsuite/gdb.base/checkpoint.c
+++ gdb/testsuite/gdb.base/checkpoint.c
@@ -1,21 +1,24 @@
 /* This testcase is part of GDB, the GNU debugger.
 
-   Copyright 2005-2013 Free Software Foundation, Inc.
+   Copyright 2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
+   the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+ 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-#include <stdlib.h>
+   Please email any bugs, comments, and/or additions to this file to:
+   bug-gdb@prep.ai.mit.edu  */
+
 #include <stdio.h>
 
 long lines = 0;
@@ -40,9 +43,9 @@ main()
   for (i = 0; ; i++)
     {
       if (ftell (in) != i)
-	fprintf (stderr, "Input error at %ld\n", i);
+	fprintf (stderr, "Input error at %d\n", i);
       if (ftell (out) != i)
-	fprintf (stderr, "Output error at %ld\n", i);
+	fprintf (stderr, "Output error at %d\n", i);
       c = fgetc (in);
       if (c == '\n')
 	lines++;	/* breakpoint 1 */
