82b6d159e87d08df943fb6d940f79817caa70e1a
mmitchel <mmitchel>
2006-02-02 06:50:07 +0000

    Backport: 	2006-02-01  Mark Mitchell  <mark@codesourcery.com> 	* sim/ppc/emul_netbsd.c (emul_netbsd_create): Quote file-name property. 	* sim/ppc/emul_unix.c (emul_unix_create): Likewise. 	* sim/ppc/tree.c (libiberty.h): Include it. 	(tree_quote_property): New function. 	* sim/ppc/tree.h (tree_quote_property): Declare.
    	Backport:
    	2006-01-23  Mark Mitchell  <mark@codesourcery.com>
    	* sim/ppc/words.h (signed32): Define as "int".
    	(unsigned32): Define as "unsigned int".

--- sim/ppc/tree.c
+++ sim/ppc/tree.c
@@ -4,7 +4,7 @@
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
+    the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
@@ -13,7 +13,8 @@
     GNU General Public License for more details.
  
     You should have received a copy of the GNU General Public License
-    along with this program; if not, see <http://www.gnu.org/licenses/>.
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  
     */
 
@@ -546,7 +547,7 @@ parse_reg_property(device *current,
   device_add_reg_array_property(current, property_name,
 				regs, nr_regs);
 
-  free(regs);
+  zfree(regs);
 }
 
 
@@ -583,7 +584,7 @@ parse_ranges_property(device *current,
   /* create it */
   device_add_range_array_property(current, property_name, ranges, nr_ranges);
 
-  free(ranges);
+  zfree(ranges);
 }
 
 
@@ -769,9 +770,9 @@ parse_string_property(device *current,
   /* flush the created string */
   while (nr_strings > 0) {
     nr_strings--;
-    free(strings[nr_strings]);
+    zfree(strings[nr_strings]);
   }
-  free(strings);
+  zfree(strings);
 }
 
 
