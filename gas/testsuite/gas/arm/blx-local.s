9418d28ad046ad02e28d34fe5821c9b3fc61fce4
pbrook <pbrook>
2006-03-20 20:53:13 +0000

    2006-03-20  Paul Brook  <paul@codesourcery.com>
    	* gas/config/tc-arm.c (md_apply_fix): Set H bit on blx instruction.
    
    	* gas/testsuite/gas/arm/blx-local.d: New test.
    	* gas/testsuite/gas/arm/blx-local.d: New test.

--- gas/testsuite/gas/arm/blx-local.s
+++ gas/testsuite/gas/arm/blx-local.s
@@ -1,38 +1,16 @@
-# objdump: -fdrw --prefix-addresses --show-raw-insn
-# not-target: *-*-*aout* *-*-pe
-
-  .text
-  .arch armv5t
-  .arm
+        .text
+	.arch armv5t
+        .arm
 one:
-        blx	foo
-	blx     foo2
-	bl	foo
-	bl	foo2
-	blx	fooundefarm
-	bl      fooundefarm
-	blx     fooundefthumb
-	bl      fooundefthumb
-	
-	.thumb
-	.type foo, %function
-	.thumb_func
-foo:
-	nop
-	nop
-fooundefthumb:
- 	nop
+        blx     foo
+        blx     foo2
 
- 	.align 2
+        .thumb
+        .type foo, %function
+        .thumb_func
+foo:
+        nop
         .type foo2, %function
-	.arm
+        .thumb_func
 foo2:
- 	bleq  fooundefthumb @no relocs
- 	beq   fooundefthumb @no relocs
- 	b     fooundefthumb @no relocs
- 	bleq  foo  @ R_ARM_PCREL_JUMP
- 	beq   foo  @ R_ARM_PCREL_JUMP
- 	b     foo  @ R_ARM_PCREL_JUMP
-	nop
-fooundefarm:
- 	nop
+        nop
