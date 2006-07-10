292c7560c1baa4c79f309e5912fd99802f9a4bae
corinna <corinna>
2006-07-10 20:39:04 +0000

    * Merge HEAD into cv-branch.
    	* fhandler.h (class fhandler_socket): Add wsock_mtx, wsock_evt
    	and wsock_events members.  Remove closed status flag, add listener
    	status flag.  Accomodate new implementation of socket event handling
    	methods.  Declare recv* and send* functions ssize_t as the POSIX
    	equivalents.
    	(fhandler_socket::recv_internal): Declare.
    	(fhandler_socket::send_internal): Ditto.
    	* fhandler_socket.cc (EVENT_MASK): Define mask of selected events.
    	(fhandler_socket::fhandler_socket): Initialize new members.
    	(fhandler_socket::af_local_setblocking): Don't actually set the
    	socket to blocking mode.  Keep sane event selection.
    	(fhandler_socket::af_local_unsetblocking): Don't actually set the
    	socket to previous blocking setting, just remember it.
    	(struct wsa_event): New structure to keep event data per shared
    	socket.
    	(NUM_SOCKS): Define number of shared sockets concurrently handled by
    	all active Cygwin processes.
    	(wsa_events): New shared datastructure keeping all wsa_event records.
    	(socket_serial_number): New shared variable to identify shared sockets.
    	(wsa_slot_mtx): Global mutex to serialize wsa_events access.
    	(search_wsa_event_slot): New static function to select a new wsa_event
    	slot for a new socket.
    	(fhandler_socket::prepare): Rewrite.  Prepare event selection
    	per new socket.
    	(fhandler_socket::wait): Rewrite.  Wait for socket events in thread
    	safe and multiple process safe.
    	(fhandler_socket::release): Rewrite.  Close per-socket descriptor
    	mutex handle and event handle.
    	(fhandler_socket::dup): Duplicate wsock_mtx and wsock_evt.  Fix
    	copy-paste error in debug output.
    	(fhandler_socket::connect): Accomodate new event handling.
    	(fhandler_socket::listen): Set listener flag on successful listen.
    	(fhandler_socket::accept): Accomodate new event handling.
    	(fhandler_socket::recv_internal): New inline method centralizing
    	common recv code.
    	(fhandler_socket::recvfrom): Call recv_internal now.
    	(fhandler_socket::recvmsg): Ditto.  Streamline copying from iovec
    	to WSABUF.
    	(fhandler_socket::send_internal): New inline method centralizing
    	common send code.
    	(fhandler_socket::sendto): Call send_internal now.
    	(fhandler_socket::sendmsg): Ditto.  Streamline copying from iovec
    	to WSABUF.
    	(fhandler_socket::close): Call release now.
    	(fhandler_socket::ioctl): Never actually switch to blocking mode.
    	Just keep track of the setting.
    	* net.cc (fdsock): Call prepare now.
    	(cygwin_connect): Revert again to event driven technique.
    	(cygwin_accept): Ditto.
    	* poll.cc (poll): Don't call recvfrom on a listening socket.
    	Remove special case for failing recvfrom.
    	* include/sys/socket.h: Declare recv* and send* functions ssize_t as
    	requested by POSIX.

--- winsup/cygwin/poll.cc
+++ winsup/cygwin/poll.cc
@@ -1,7 +1,6 @@
 /* poll.cc. Implements poll(2) via usage of select(2) call.
 
-   Copyright 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2011,
-   2012 Red Hat, Inc.
+   Copyright 2000, 2001, 2002, 2003, 2004, 2005 Red Hat, Inc.
 
    This file is part of Cygwin.
 
@@ -13,22 +12,24 @@
 
 #define FD_SETSIZE 16384		// lots of fds
 #include "winsup.h"
+#include <sys/time.h>
 #include <sys/poll.h>
+#include <sys/socket.h>
 #include <stdlib.h>
 #define USE_SYS_TYPES_FD_SET
+#include <winsock2.h>
 #include "cygerrno.h"
+#include "security.h"
 #include "path.h"
 #include "fhandler.h"
 #include "dtable.h"
 #include "cygheap.h"
-#include "pinfo.h"
 #include "sigproc.h"
 
 extern "C" int
-poll (struct pollfd *fds, nfds_t nfds, int timeout)
+poll (struct pollfd *fds, unsigned int nfds, int timeout)
 {
   int max_fd = 0;
-  int invalid_fds = 0;
   fd_set *read_fds, *write_fds, *except_fds;
   struct timeval tv = { timeout / 1000, (timeout % 1000) * 1000 };
 
@@ -52,6 +53,7 @@ poll (struct pollfd *fds, nfds_t nfds, int timeout)
   memset (write_fds, 0, fds_size);
   memset (except_fds, 0, fds_size);
 
+  int invalid_fds = 0;
   for (unsigned int i = 0; i < nfds; ++i)
     {
       fds[i].revents = 0;
@@ -61,7 +63,9 @@ poll (struct pollfd *fds, nfds_t nfds, int timeout)
 	    FD_SET(fds[i].fd, read_fds);
 	  if (fds[i].events & POLLOUT)
 	    FD_SET(fds[i].fd, write_fds);
-	  if (fds[i].events & POLLPRI)
+	  /* On sockets, except_fds is needed to catch failed connects. */
+	  if ((fds[i].events & POLLPRI)
+	      || cygheap->fdtab[fds[i].fd]->is_socket ())
 	    FD_SET(fds[i].fd, except_fds);
 	}
       else if (fds[i].fd >= 0)
@@ -71,90 +75,72 @@ poll (struct pollfd *fds, nfds_t nfds, int timeout)
 	}
     }
 
-  /* Invalid fds? */
-  if (invalid_fds > 0)
-    {
-      /* Only invalid fds?  Return. */
-      if ((nfds_t) invalid_fds == nfds)
-	return invalid_fds;
-      /* POSIX doesn't explicitely define this behaviour, but on Linux,
-	 the timeout is set to 0 if an error occurs, and POLLNVAL is one
-	 of these errors.  So, for Linux-compatibility's sake... */
-      tv.tv_sec = tv.tv_usec = 0;
-    }
+  if (invalid_fds)
+    return invalid_fds;
 
-  int ret = cygwin_select (max_fd + 1, read_fds, write_fds, except_fds,
-			   timeout < 0 ? NULL : &tv);
-  /* timeout, signal, whatever?  Return.  If invalid fds exist, return with
-     their number. */
-  if (ret <= 0)
-    return invalid_fds ?: ret;
+  int ret = cygwin_select (max_fd + 1, read_fds, write_fds, except_fds, timeout < 0 ? NULL : &tv);
 
-  /* Set revents fields and count fds with non-zero revents fields for
-     return value. */
-  ret = 0;
-  for (unsigned int i = 0; i < nfds; ++i)
-    {
-      if (fds[i].fd >= 0 && fds[i].revents != POLLNVAL)
-	{
-	  fhandler_socket *sock;
+  if (ret > 0)
+    for (unsigned int i = 0; i < nfds; ++i)
+      {
+	if (fds[i].fd >= 0)
+	  {
+	    if (cygheap->fdtab.not_open (fds[i].fd))
+	      fds[i].revents = POLLHUP;
+	    else
+	      {
+		fhandler_socket *sock;
 
-	  /* Check if the descriptor has been closed, or if shutdown for the
-	     read side has been called on a socket. */
-	  if (cygheap->fdtab.not_open (fds[i].fd)
-	      || ((sock = cygheap->fdtab[fds[i].fd]->is_socket ())
-		  && sock->saw_shutdown_read ()))
-	    fds[i].revents = POLLHUP;
-	  else
-	    {
-	      if (FD_ISSET(fds[i].fd, read_fds))
-		/* This should be sufficient for sockets, too.  Using
-		   MSG_PEEK, as before, can be considered dangerous at
-		   best.  Quote from W. Richard Stevens: "The presence
-		   of an error can be considered either normal data or
-		   an error (POLLERR).  In either case, a subsequent read
-		   will return -1 with errno set to the appropriate value."
-		   So it looks like there's actually no good reason to
-		   return POLLERR. */
-		fds[i].revents |= POLLIN;
-	      /* Handle failed connect. */
-	      if (FD_ISSET(fds[i].fd, write_fds)
-		  && (sock = cygheap->fdtab[fds[i].fd]->is_socket ())
-		  && sock->connect_state () == connect_failed)
-		fds[i].revents |= (POLLIN | POLLERR);
-	      else
-		{
-		  if (FD_ISSET(fds[i].fd, write_fds))
-		    fds[i].revents |= POLLOUT;
-		  if (FD_ISSET(fds[i].fd, except_fds))
-		    fds[i].revents |= POLLPRI;
-		}
-	    }
-	  if (fds[i].revents)
-	    ++ret;
-	}
-    }
-  /* Number of fds with event includes the invalid fds. */
-  return ret + invalid_fds;
-}
+		if (FD_ISSET(fds[i].fd, read_fds))
+		  {
+		    char peek[1];
+		    sock = cygheap->fdtab[fds[i].fd]->is_socket ();
+		    if (!sock)
+		      fds[i].revents |= POLLIN;
+		    else if (sock->listener ())
+		      {
+			fds[i].revents |= POLLIN;
+		      }
+		    else
+		      {
+			/* The following action can change errno.  We have to
+			   reset it to it's old value. */
+			int old_errno = get_errno ();
+			switch (sock->recvfrom (peek, sizeof (peek), MSG_PEEK,
+						NULL, NULL))
+			  {
+			    case -1:
+			      fds[i].revents |= POLLERR;
+			      break;
+			    case 0:  /* Closed on the read side... */
+			      /* ...or shutdown(SHUT_WR) on the write side.
+			         We set revents to POLLHUP until 1.5.18, but
+				 this is semantically borderline. */
+			      fds[i].revents |= POLLIN;
+			      break;
+			    default:
+			      fds[i].revents |= POLLIN;
+			      break;
+			  }
+			set_errno (old_errno);
+		      }
+		  }
+		/* Handle failed connect. */
+		if (FD_ISSET(fds[i].fd, write_fds)
+		    && FD_ISSET(fds[i].fd, except_fds)
+		    && (sock = cygheap->fdtab[fds[i].fd]->is_socket ())
+		    && sock->connect_state () == connect_failed)
+		  fds[i].revents |= (POLLIN | POLLERR);
+		else
+		  {
+		    if (FD_ISSET(fds[i].fd, write_fds))
+		      fds[i].revents |= POLLOUT;
+		    if (FD_ISSET(fds[i].fd, except_fds))
+		      fds[i].revents |= POLLPRI;
+		  }
+	      }
+	  }
+      }
 
-extern "C" int
-ppoll (struct pollfd *fds, nfds_t nfds, const struct timespec *timeout_ts,
-       const sigset_t *sigmask)
-{
-  int timeout;
-  sigset_t oldset = _my_tls.sigmask;
-
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-  timeout = (timeout_ts == NULL)
-	    ? -1
-	    : (timeout_ts->tv_sec * 1000 + timeout_ts->tv_nsec / 1000000);
-  if (sigmask)
-    set_signal_mask (_my_tls.sigmask, *sigmask);
-  int ret = poll (fds, nfds, timeout);
-  if (sigmask)
-    set_signal_mask (_my_tls.sigmask, oldset);
   return ret;
 }
