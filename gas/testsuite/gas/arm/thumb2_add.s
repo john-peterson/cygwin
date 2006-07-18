f8b571cf4c51a8ee08ebebb4687e1a612aa48375
pbrook <pbrook>
2006-07-18 16:45:51 +0000

    2006-07-18  Paul Brook  <paul@codesourcery.com>
    	Merge from mainline.
    	bfd/
    	* bfd-in2.h: Regenerate.
    	* libbfd.h: Regenerate.
    	* reloc.c: Add BFD_RELOC_ARM_T32_ADD_IMM.
    
    	gas/
    	* tc-arm.c (do_t_add_sub): Use addw/subw when source is PC.
    	(md_convert_frag): Use correct reloc for add_pc.  Use
    	BFD_RELOC_ARM_T32_ADD_IMM for normal add/sum.
    	(md_apply_fix): Handle BFD_RELOC_ARM_T32_ADD_IMM.
    	(arm_force_relocation): Handle BFD_RELOC_ARM_T32_ADD_IMM.
    
    	gas/testsuite/
    	* gas/arm/thumb2_add.d: New test.
    	* gas/arm/thumb2_add.s: New test.

--- gas/testsuite/gas/arm/thumb2_add.s
+++ gas/testsuite/gas/arm/thumb2_add.s
@@ -4,6 +4,7 @@
 	.global	thumb2_add
 	.thumb
 	.thumb_func
+	.type	thumb2_add, %function
 thumb2_add:
 	add r0, pc, #0x800
 	add r9, pc, #0
@@ -17,15 +18,3 @@ thumb2_add:
 	sub r8, r9, #0x400
 	sub r8, r9, #0x101
 	sub r3, r1, #0x101
-	add r3, #1
-	sub r3, #1
-	sub sp, sp, #0x100
-	sub sp, sp, #0x200
-	sub sp, sp, #0x101
-	add sp, sp, #0x100
-	add sp, sp, #0x200
-	add sp, sp, #0x101
-	add r0, sp, #0x100
-	add r5, sp, #0x400
-	add r9, sp, #0x101
-	rsbs r1, r6, #0
