3b84225d348e31d06e8381bedd52cc2f351720a3
amodra <amodra>
2007-08-15 04:33:59 +0000

    bfd/ 	* elf64-ppc.c (ADDI_R12_R12, LD_R11_0R2, LD_R2_0R2): Define. 	Update stub comments. 	(build_plt_stub): Build two variants, one without "addis". 	(ppc_build_one_stub): Build stubs without "addis" if possible. 	(ppc_size_one_stub): Size new stubs. ld/testsuite/ 	* ld-powerpc/relbrlt.s (.text.pad2): Adjust space. 	* ld-powerpc/relbrlt.d: Update. 	* ld-powerpc/tlsexe.d: Update. 	* ld-powerpc/tlsexe.g: Update. 	* ld-powerpc/tlsexe.r: Update. 	* ld-powerpc/tlsexetoc.d: Update. 	* ld-powerpc/tlsexetoc.g: Update. 	* ld-powerpc/tlsexetoc.r: Update. 	* ld-powerpc/tlsso.d: Update. 	* ld-powerpc/tlsso.g: Update. 	* ld-powerpc/tlsso.r: Update. 	* ld-powerpc/tlstocso.d: Update. 	* ld-powerpc/tlstocso.g: Update.

--- ld/testsuite/ld-powerpc/tlsexetoc.r
+++ ld/testsuite/ld-powerpc/tlsexetoc.r
@@ -1,35 +1,36 @@
 #source: tlslib.s
 #source: tlstoc.s
 #as: -a64
-#ld: 
+#ld: -melf64ppc
 #readelf: -WSsrl
 #target: powerpc64*-*-*
 
-There are [0-9]+ section headers, starting at offset 0x[0-9a-f]+:
+There are 17 section headers.*
 
 Section Headers:
  +\[Nr\] Name +Type +Address +Off +Size +ES Flg Lk Inf Al
- +\[[ 0-9]+\] +NULL +0+ 0+ 0+ 0+ +0 +0 +0
- +\[[ 0-9]+\] \.interp +.*
- +\[[ 0-9]+\] \.hash +.*
- +\[[ 0-9]+\] \.dynsym +.*
- +\[[ 0-9]+\] \.dynstr +.*
- +\[[ 0-9]+\] \.rela\.dyn +.*
- +\[[ 0-9]+\] \.rela\.plt +.*
- +\[[ 0-9]+\] \.text +PROGBITS .* 0+e8 0+ +AX +0 +0 +32
- +\[[ 0-9]+\] \.tdata +PROGBITS .* 0+38 0+ WAT +0 +0 +8
- +\[[ 0-9]+\] \.tbss +NOBITS .* 0+38 0+ WAT +0 +0 +8
- +\[[ 0-9]+\] \.dynamic +DYNAMIC .* 0+160 10 +WA +4 +0 +8
- +\[[ 0-9]+\] \.got +PROGBITS .* 0+58 08 +WA +0 +0 +8
- +\[[ 0-9]+\] \.plt +.*
- +\[[ 0-9]+\] \.shstrtab +.*
- +\[[ 0-9]+\] \.symtab +.*
- +\[[ 0-9]+\] \.strtab +.*
+ +\[ 0\] +NULL +0+ 0+ 0+ 0+ +0 +0 +0
+ +\[ 1\] \.interp +.*
+ +\[ 2\] \.hash +.*
+ +\[ 3\] \.dynsym +.*
+ +\[ 4\] \.dynstr +.*
+ +\[ 5\] \.rela\.dyn +.*
+ +\[ 6\] \.rela\.plt +.*
+ +\[ 7\] \.text +PROGBITS .* 0+b8 0+ +AX +0 +0 +8
+ +\[ 8\] \.tdata +PROGBITS .* 0+38 0+ WAT +0 +0 +8
+ +\[ 9\] \.tbss +NOBITS .* 0+38 0+ WAT +0 +0 +8
+ +\[10\] \.dynamic +DYNAMIC .* 0+150 10 +WA +4 +0 +8
+ +\[11\] \.branch_lt +PROGBITS .* 0+ 0+ +WA +0 +0 +8
+ +\[12\] \.got +PROGBITS .* 0+58 08 +WA +0 +0 +8
+ +\[13\] \.plt +.*
+ +\[14\] \.shstrtab +.*
+ +\[15\] \.symtab +.*
+ +\[16\] \.strtab +.*
 #...
 
 Elf file type is EXEC \(Executable file\)
 Entry point .*
-There are [0-9]+ program headers, starting at offset [0-9]+
+There are 6 program headers.*
 
 Program Headers:
  +Type +Offset +VirtAddr +PhysAddr +FileSiz +MemSiz +Flg Align
@@ -58,34 +59,34 @@ Relocation section '\.rela\.dyn' at offset .* contains 3 entries:
 
 Relocation section '\.rela\.plt' at offset .* contains 1 entries:
  +Offset +Info +Type +Symbol's Value +Symbol's Name \+ Addend
-[0-9a-f ]+R_PPC64_JMP_SLOT +0+ __tls_get_addr_opt \+ 0
+[0-9a-f ]+R_PPC64_JMP_SLOT +0+ __tls_get_addr \+ 0
 
 Symbol table '\.dynsym' contains [0-9]+ entries:
- +Num: +Value +Size +Type +Bind +Vis +Ndx +Name
+ +Num: +Value +Size Type +Bind +Vis +Ndx Name
 .* NOTYPE +LOCAL +DEFAULT +UND 
-.* TLS +GLOBAL +DEFAULT +UND gd
-.* TLS +GLOBAL +DEFAULT +UND ld
-.* NOTYPE +GLOBAL +DEFAULT +12 __bss_start
-.* FUNC +GLOBAL +DEFAULT +UND __tls_get_addr_opt
-.* NOTYPE +GLOBAL +DEFAULT +11 _edata
-.* NOTYPE +GLOBAL +DEFAULT +12 _end
+.* TLS +GLOBAL DEFAULT +UND gd
+.* FUNC +GLOBAL DEFAULT +UND __tls_get_addr
+.* TLS +GLOBAL DEFAULT +UND ld
+.* NOTYPE +GLOBAL DEFAULT +ABS __bss_start
+.* NOTYPE +GLOBAL DEFAULT +ABS _edata
+.* NOTYPE +GLOBAL DEFAULT +ABS _end
 
-Symbol table '\.symtab' contains [0-9]+ entries:
- +Num: +Value +Size +Type +Bind +Vis +Ndx +Name
+Symbol table '\.symtab' contains .* entries:
+ +Num: +Value +Size Type +Bind +Vis +Ndx Name
 .* NOTYPE +LOCAL +DEFAULT +UND 
-.* SECTION +LOCAL +DEFAULT +1 
-.* SECTION +LOCAL +DEFAULT +2 
-.* SECTION +LOCAL +DEFAULT +3 
-.* SECTION +LOCAL +DEFAULT +4 
-.* SECTION +LOCAL +DEFAULT +5 
-.* SECTION +LOCAL +DEFAULT +6 
-.* SECTION +LOCAL +DEFAULT +7 
-.* SECTION +LOCAL +DEFAULT +8 
-.* SECTION +LOCAL +DEFAULT +9 
-.* SECTION +LOCAL +DEFAULT +10 
-.* SECTION +LOCAL +DEFAULT +11 
-.* SECTION +LOCAL +DEFAULT +12 
-.* FILE +LOCAL +DEFAULT +ABS .*
+.* SECTION LOCAL +DEFAULT +1 
+.* SECTION LOCAL +DEFAULT +2 
+.* SECTION LOCAL +DEFAULT +3 
+.* SECTION LOCAL +DEFAULT +4 
+.* SECTION LOCAL +DEFAULT +5 
+.* SECTION LOCAL +DEFAULT +6 
+.* SECTION LOCAL +DEFAULT +7 
+.* SECTION LOCAL +DEFAULT +8 
+.* SECTION LOCAL +DEFAULT +9 
+.* SECTION LOCAL +DEFAULT +10 
+.* SECTION LOCAL +DEFAULT +11 
+.* SECTION LOCAL +DEFAULT +12 
+.* SECTION LOCAL +DEFAULT +13 
 .* TLS +LOCAL +DEFAULT +8 gd4
 .* TLS +LOCAL +DEFAULT +8 ld4
 .* TLS +LOCAL +DEFAULT +8 ld5
@@ -93,23 +94,20 @@ Symbol table '\.symtab' contains [0-9]+ entries:
 .* TLS +LOCAL +DEFAULT +8 ie4
 .* TLS +LOCAL +DEFAULT +8 le4
 .* TLS +LOCAL +DEFAULT +8 le5
-.* NOTYPE +LOCAL +DEFAULT +11 \.Lie0
-.* (FUNC|NOTYPE) +LOCAL +DEFAULT +UND \.__tls_get_addr(|_opt)
-.* FILE +LOCAL +DEFAULT +ABS .*
-.* NOTYPE +LOCAL +DEFAULT +7 00000010\.plt_call\.__tls_get_addr(|_opt)
-.* OBJECT +LOCAL +DEFAULT +10 _DYNAMIC
-.* NOTYPE +LOCAL +DEFAULT +7 __glink_PLTresolve
-.* TLS +GLOBAL +DEFAULT +UND gd
-.* TLS +GLOBAL +DEFAULT +9 le0
-.* TLS +GLOBAL +DEFAULT +9 ld0
-.* TLS +GLOBAL +DEFAULT +9 le1
-.* TLS +GLOBAL +DEFAULT +UND ld
-.* NOTYPE +GLOBAL +DEFAULT +7 _start
-.* TLS +GLOBAL +DEFAULT +9 ld2
-.* TLS +GLOBAL +DEFAULT +9 ld1
-.* NOTYPE +GLOBAL +DEFAULT +12 __bss_start
-.* FUNC +GLOBAL +DEFAULT +UND __tls_get_addr_opt
-.* NOTYPE +GLOBAL +DEFAULT +11 _edata
-.* NOTYPE +GLOBAL +DEFAULT +12 _end
-.* TLS +GLOBAL +DEFAULT +9 gd0
-.* TLS +GLOBAL +DEFAULT +9 ie0
+.* NOTYPE +LOCAL +DEFAULT +12 \.Lie0
+.* OBJECT +LOCAL +HIDDEN +10 _DYNAMIC
+.* FUNC +LOCAL +DEFAULT +UND \.__tls_get_addr
+.* TLS +GLOBAL DEFAULT +UND gd
+.* TLS +GLOBAL DEFAULT +9 le0
+.* FUNC +GLOBAL DEFAULT +UND __tls_get_addr
+.* TLS +GLOBAL DEFAULT +9 ld0
+.* TLS +GLOBAL DEFAULT +9 le1
+.* TLS +GLOBAL DEFAULT +UND ld
+.* NOTYPE +GLOBAL DEFAULT +7 _start
+.* TLS +GLOBAL DEFAULT +9 ld2
+.* TLS +GLOBAL DEFAULT +9 ld1
+.* NOTYPE +GLOBAL DEFAULT +ABS __bss_start
+.* NOTYPE +GLOBAL DEFAULT +ABS _edata
+.* NOTYPE +GLOBAL DEFAULT +ABS _end
+.* TLS +GLOBAL DEFAULT +9 gd0
+.* TLS +GLOBAL DEFAULT +9 ie0
