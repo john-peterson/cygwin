b00718a1acc8f2c17d4ceba8584f8380f5d895e4
corinna <corinna>
2007-12-13 17:19:46 +0000

    * poll.cc (poll): Return count of fds with events instead of total 	event count.
    	* poll.cc (poll): Use POSIX type nfds_t for second parameter.
    	* include/sys/poll.h: Declare nfds_t.  Use as type for second parameter
    	in poll(2) declaration.

--- winsup/cygwin/poll.cc
+++ winsup/cygwin/poll.cc
@@ -1,7 +1,6 @@
 /* poll.cc. Implements poll(2) via usage of select(2) call.
 
-   Copyright 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2011,
-   2012 Red Hat, Inc.
+   Copyright 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 Red Hat, Inc.
 
    This file is part of Cygwin.
 
@@ -13,22 +12,24 @@
 
 #define FD_SETSIZE 16384		// lots of fds
 #include "winsup.h"
+#include <sys/time.h>
 #include <sys/poll.h>
+#include <sys/socket.h>
 #include <stdlib.h>
 #define USE_SYS_TYPES_FD_SET
+#include <winsock2.h>
 #include "cygerrno.h"
+#include "security.h"
 #include "path.h"
 #include "fhandler.h"
 #include "dtable.h"
 #include "cygheap.h"
-#include "pinfo.h"
 #include "sigproc.h"
 
 extern "C" int
 poll (struct pollfd *fds, nfds_t nfds, int timeout)
 {
   int max_fd = 0;
-  int invalid_fds = 0;
   fd_set *read_fds, *write_fds, *except_fds;
   struct timeval tv = { timeout / 1000, (timeout % 1000) * 1000 };
 
@@ -52,6 +53,7 @@ poll (struct pollfd *fds, nfds_t nfds, int timeout)
   memset (write_fds, 0, fds_size);
   memset (except_fds, 0, fds_size);
 
+  int invalid_fds = 0;
   for (unsigned int i = 0; i < nfds; ++i)
     {
       fds[i].revents = 0;
@@ -61,7 +63,9 @@ poll (struct pollfd *fds, nfds_t nfds, int timeout)
 	    FD_SET(fds[i].fd, read_fds);
 	  if (fds[i].events & POLLOUT)
 	    FD_SET(fds[i].fd, write_fds);
-	  if (fds[i].events & POLLPRI)
+	  /* On sockets, except_fds is needed to catch failed connects. */
+	  if ((fds[i].events & POLLPRI)
+	      || cygheap->fdtab[fds[i].fd]->is_socket ())
 	    FD_SET(fds[i].fd, except_fds);
 	}
       else if (fds[i].fd >= 0)
@@ -71,54 +75,70 @@ poll (struct pollfd *fds, nfds_t nfds, int timeout)
 	}
     }
 
-  /* Invalid fds? */
-  if (invalid_fds > 0)
-    {
-      /* Only invalid fds?  Return. */
-      if ((nfds_t) invalid_fds == nfds)
-	return invalid_fds;
-      /* POSIX doesn't explicitely define this behaviour, but on Linux,
-	 the timeout is set to 0 if an error occurs, and POLLNVAL is one
-	 of these errors.  So, for Linux-compatibility's sake... */
-      tv.tv_sec = tv.tv_usec = 0;
-    }
+  if (invalid_fds)
+    return invalid_fds;
 
   int ret = cygwin_select (max_fd + 1, read_fds, write_fds, except_fds,
 			   timeout < 0 ? NULL : &tv);
-  /* timeout, signal, whatever?  Return.  If invalid fds exist, return with
-     their number. */
   if (ret <= 0)
-    return invalid_fds ?: ret;
+    return ret;
 
   /* Set revents fields and count fds with non-zero revents fields for
      return value. */
   ret = 0;
   for (unsigned int i = 0; i < nfds; ++i)
     {
-      if (fds[i].fd >= 0 && fds[i].revents != POLLNVAL)
+      if (fds[i].fd >= 0)
 	{
-	  fhandler_socket *sock;
-
-	  /* Check if the descriptor has been closed, or if shutdown for the
-	     read side has been called on a socket. */
-	  if (cygheap->fdtab.not_open (fds[i].fd)
-	      || ((sock = cygheap->fdtab[fds[i].fd]->is_socket ())
-		  && sock->saw_shutdown_read ()))
+	  if (cygheap->fdtab.not_open (fds[i].fd))
 	    fds[i].revents = POLLHUP;
 	  else
 	    {
+	      fhandler_socket *sock;
+
 	      if (FD_ISSET(fds[i].fd, read_fds))
-		/* This should be sufficient for sockets, too.  Using
-		   MSG_PEEK, as before, can be considered dangerous at
-		   best.  Quote from W. Richard Stevens: "The presence
-		   of an error can be considered either normal data or
-		   an error (POLLERR).  In either case, a subsequent read
-		   will return -1 with errno set to the appropriate value."
-		   So it looks like there's actually no good reason to
-		   return POLLERR. */
-		fds[i].revents |= POLLIN;
+		{
+		  char peek[1];
+		  sock = cygheap->fdtab[fds[i].fd]->is_socket ();
+		  if (!sock)
+		    fds[i].revents |= POLLIN;
+		  else
+		    {
+		      /* The following action can change errno.  We have to
+			 reset it to it's old value. */
+		      int old_errno = get_errno ();
+		      switch (sock->recvfrom (peek, sizeof (peek), MSG_PEEK,
+					      NULL, NULL))
+			{
+			  case -1: /* Something weird happened */
+			    /* When select returns that data is available,
+			       that could mean that the socket is in
+			       listen mode and a client tries to connect.
+			       Unfortunately, recvfrom() doesn't make much
+			       sense then.  It returns WSAENOTCONN in that
+			       case.  Since that's not actually an error,
+			       we must not set POLLERR but POLLIN. */
+			    if (WSAGetLastError () != WSAENOTCONN)
+			      fds[i].revents |= POLLERR;
+			    else
+			      fds[i].revents |= POLLIN;
+			    break;
+			  case 0:  /* Closed on the read side... */
+			    /* ...or shutdown(SHUT_WR) on the write side.
+			       We set revents to POLLHUP until 1.5.18, but
+			       this is semantically borderline. */
+			    fds[i].revents |= POLLIN;
+			    break;
+			  default:
+			    fds[i].revents |= POLLIN;
+			    break;
+			}
+		      set_errno (old_errno);
+		    }
+		}
 	      /* Handle failed connect. */
 	      if (FD_ISSET(fds[i].fd, write_fds)
+		  && FD_ISSET(fds[i].fd, except_fds)
 		  && (sock = cygheap->fdtab[fds[i].fd]->is_socket ())
 		  && sock->connect_state () == connect_failed)
 		fds[i].revents |= (POLLIN | POLLERR);
@@ -134,27 +154,6 @@ poll (struct pollfd *fds, nfds_t nfds, int timeout)
 	    ++ret;
 	}
     }
-  /* Number of fds with event includes the invalid fds. */
-  return ret + invalid_fds;
-}
 
-extern "C" int
-ppoll (struct pollfd *fds, nfds_t nfds, const struct timespec *timeout_ts,
-       const sigset_t *sigmask)
-{
-  int timeout;
-  sigset_t oldset = _my_tls.sigmask;
-
-  myfault efault;
-  if (efault.faulted (EFAULT))
-    return -1;
-  timeout = (timeout_ts == NULL)
-	    ? -1
-	    : (timeout_ts->tv_sec * 1000 + timeout_ts->tv_nsec / 1000000);
-  if (sigmask)
-    set_signal_mask (_my_tls.sigmask, *sigmask);
-  int ret = poll (fds, nfds, timeout);
-  if (sigmask)
-    set_signal_mask (_my_tls.sigmask, oldset);
   return ret;
 }
