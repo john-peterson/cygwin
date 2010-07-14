b2edf2aad93f8a744e40b34599278580350ec783
tromey <tromey>
2010-07-14 14:18:03 +0000

    2010-07-13  Emmanuel Thomé  <Emmanuel.Thome@gmail.com>
    	* c-valprint.c (c_val_print): Add embedded_offset to address in
    	call to val_print_array_elements.
    
    2010-07-13  Tom Tromey  <tromey@redhat.com>
    
    	* gdb.python/py-prettyprint.c (struct arraystruct): New struct.
    	(main): Use it.
    	* gdb.python/py-prettyprint.exp (run_lang_tests): Add test.

--- gdb/testsuite/gdb.python/py-prettyprint.c
+++ gdb/testsuite/gdb.python/py-prettyprint.c
@@ -1,6 +1,6 @@
 /* This testcase is part of GDB, the GNU debugger.
 
-   Copyright 2008-2013 Free Software Foundation, Inc.
+   Copyright 2008, 2009, 2010 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -42,14 +42,6 @@ struct ns {
 
 struct lazystring {
   const char *lazy_str;
-};
-
-struct hint_error {
-  int x;
-};
-
-struct children_as_list {
-  int x;
 };
 
 #ifdef __cplusplus
@@ -98,16 +90,6 @@ class Derived : public Vbase1, public Vbase2, public Vbase3
     }
 };
 
-class Fake
-{
-  int sname;
-  
- public:
-  Fake (const int name = 0):
-  sname (name)
-  {
-  }
-};
 #endif
 
 struct substruct {
@@ -153,11 +135,6 @@ struct justchildren
 
 typedef struct justchildren nostring_type;
 
-struct memory_error
-{
-  const char *s;
-};
-
 struct container
 {
   string name;
@@ -196,13 +173,6 @@ add_item (zzz_type *c, int val)
   ++c->len;
 }
 
-void
-set_item(zzz_type *c, int i, int val)
-{
-  if (i < c->len)
-    c->elements[i] = val;
-}
-
 void init_s(struct s *s, int a)
 {
   s->a = a;
@@ -230,31 +200,6 @@ struct nullstr
 struct string_repr string_1 = { { "one" } };
 struct string_repr string_2 = { { "two" } };
 
-static int
-eval_func (int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8)
-{
-  return p1;
-}
-
-static void
-eval_sub (void)
-{
-  struct eval_type_s { int x; } eval1 = { 1 }, eval2 = { 2 }, eval3 = { 3 },
-				eval4 = { 4 }, eval5 = { 5 }, eval6 = { 6 },
-				eval7 = { 7 }, eval8 = { 8 }, eval9 = { 9 };
-
-  eval1.x++; /* eval-break */
-}
-
-static void
-bug_14741()
-{
-  zzz_type c = make_container ("bug_14741");
-  add_item (&c, 71);
-  set_item(&c, 0, 42); /* breakpoint bug 14741 */
-  set_item(&c, 0, 5);
-}
-
 int
 main ()
 {
@@ -267,17 +212,9 @@ main ()
   const struct string_repr cstring = { { "const string" } };
   /* Clearing by being `static' could invoke an other GDB C++ bug.  */
   struct nullstr nullstr;
-  nostring_type nstype, nstype2;
-  struct memory_error me;
-  struct ns ns, ns2;
-  struct lazystring estring, estring2;
-  struct hint_error hint_error;
-  struct children_as_list children_as_list;
-
+  nostring_type nstype;
   nstype.elements = narray;
   nstype.len = 0;
-
-  me.s = "blah";
 
   init_ss(&ss, 1, 2);
   init_ss(ssa+0, 3, 4);
@@ -288,17 +225,12 @@ main ()
   init_s (&arraystruct.x[0], 23);
   init_s (&arraystruct.x[1], 24);
 
+  struct ns  ns;
   ns.null_str = "embedded\0null\0string";
   ns.length = 20;
 
-  /* Make a "corrupted" string.  */
-  ns2.null_str = NULL;
-  ns2.length = 20;
-
+  struct lazystring estring;
   estring.lazy_str = "embedded x\201\202\203\204" ;
-
-  /* Incomplete UTF-8, but ok Latin-1.  */
-  estring2.lazy_str = "embedded x\302";
 
 #ifdef __cplusplus
   S cps;
@@ -322,7 +254,6 @@ main ()
 
   Derived derived;
   
-  Fake fake (42);
 #endif
 
   add_item (&c, 23);		/* MI breakpoint here */
@@ -344,10 +275,5 @@ main ()
   nstype.elements[1] = 42;
   nstype.len = 2;
   
-  nstype2 = nstype;
-
-  eval_sub ();
-
-  bug_14741();      /* break to inspect struct and union */
-  return 0;
+  return 0;      /* break to inspect struct and union */
 }
