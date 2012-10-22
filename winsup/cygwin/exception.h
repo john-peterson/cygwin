07bebb30b9266117e790604de90c2937ef0a938e
corinna <corinna>
2012-10-22 18:34:04 +0000

    * cygtls.h (_tlsbase): Define for __x86_64__. 	(_tlstop): Ditto. 	* exception.h (exception_list): Ditto.

--- winsup/cygwin/exception.h
+++ winsup/cygwin/exception.h
@@ -1,6 +1,6 @@
 /* exception.h
 
-   Copyright 2010, 2011, 2012, 2013 Red Hat, Inc.
+   Copyright 2010, 2011, 2012 Red Hat, Inc.
 
 This software is a copyrighted work licensed under the terms of the
 Cygwin license.  Please consult the file "CYGWIN_LICENSE" for
@@ -10,7 +10,11 @@ details. */
 
 #include <exceptions.h>
 
+#ifdef __x86_64__
+extern exception_list *_except_list asm ("%gs:0");
+#else
 extern exception_list *_except_list asm ("%fs:0");
+#endif
 
 class exception
 {
@@ -28,15 +32,10 @@ public:
   ~exception () __attribute__ ((always_inline)) { _except_list = save; }
 };
 
-class cygwin_exception
+void stackdump (DWORD, CONTEXT * = NULL, EXCEPTION_RECORD * = NULL);
+extern void inline
+stackdump (DWORD n, bool)
 {
-  DWORD ebp;
-  PCONTEXT ctx;
-  EXCEPTION_RECORD *e;
-  void dump_exception ();
-public:
-  cygwin_exception (DWORD in_ebp, PCONTEXT in_ctx = NULL, EXCEPTION_RECORD *in_e = NULL):
-    ebp (in_ebp), ctx (in_ctx), e (in_e) {}
-  void dumpstack ();
-  PCONTEXT context () const {return ctx;}
-};
+  stackdump (n, (CONTEXT *) 1);
+}
+
